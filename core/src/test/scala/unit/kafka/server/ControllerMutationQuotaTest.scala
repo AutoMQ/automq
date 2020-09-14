AutoMQ/automq

commit f02e092652012928e4b82fd93ea2105e5133706b

KAFKA-10162; Use Token Bucket algorithm for controller mutation quota (KIP-599, Part III) (#9114)
Based on the discussion in #9072, I have put together an alternative way. This one does the following:

Instead of changing the implementation of the Rate to behave like a Token Bucket, it actually use two different metrics: the regular Rate and a new Token Bucket. The latter is used to enforce the quota.
The Token Bucket algorithm uses the rate of the quota as the refill rate for the credits and compute the burst based on the number of samples and their length (# samples * sample length * quota).
The Token Bucket algorithm used can go under zero in order to handle unlimited burst (e.g. create topic with a number of partitions higher than the burst). Throttling kicks in when the number of credits is under zero.
The throttle time is computed as credits under zero / refill rate (or quota).
Only the controller mutation uses it for now.
The remaining number of credits in the bucket is exposed with the tokens metrics per user/clientId.

Reviewers: Anna Povzner <anna@confluent.io>, Jun Rao <junrao@gmail.com>

clients/src/main/java/org/apache/kafka/common/metrics/Sensor.java
@@ -18,6 +18,7 @@ package org.apache.kafka.common.metrics;
 
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.metrics.CompoundStat.NamedMeasurable;
+import org.apache.kafka.common.metrics.stats.TokenBucket;
 import org.apache.kafka.common.utils.Time;
 
 import java.util.ArrayList;
@@ -43,13 +44,23 @@ public final class Sensor {
     private final Metrics registry;
     private final String name;
     private final Sensor[] parents;
-    private final List<Stat> stats;
+    private final List<StatAndConfig> stats;
     private final Map<MetricName, KafkaMetric> metrics;
     private final MetricConfig config;
     private final Time time;
     private volatile long lastRecordTime;
     private final long inactiveSensorExpirationTimeMs;
     private final Object metricLock;
+
+    private static class StatAndConfig {
+        public final Stat stat;
+        public final MetricConfig config;
+
+        StatAndConfig(Stat stat, MetricConfig config) {
+            this.stat = stat;
+            this.config = config;
+        }
+    }
 
     public enum RecordingLevel {
         INFO(0, "INFO"), DEBUG(1, "DEBUG");
@@ -199,8 +210,9 @@ public final class Sensor {
         synchronized (this) {
             synchronized (metricLock()) {
                 // increment all the stats
-                for (Stat stat : this.stats)
-                    stat.record(config, value, timeMs);
+                for (StatAndConfig statAndConfig : this.stats) {
+                    statAndConfig.stat.record(statAndConfig.config, value, timeMs);
+                }
             }
             if (checkQuotas)
                 checkQuotas(timeMs);
@@ -223,8 +235,14 @@ public final class Sensor {
                 Quota quota = config.quota();
                 if (quota != null) {
                     double value = metric.measurableValue(timeMs);
-                    if (!quota.acceptable(value)) {
-                        throw new QuotaViolationException(metric, value, quota.bound());
+                    if (metric.measurable() instanceof TokenBucket) {
+                        if (value < 0) {
+                            throw new QuotaViolationException(metric, value, quota.bound());
+                        }
+                    } else {
+                        if (!quota.acceptable(value)) {
+                            throw new QuotaViolationException(metric, value, quota.bound());
+                        }
                     }
                 }
             }
@@ -251,10 +269,11 @@ public final class Sensor {
         if (hasExpired())
             return false;
 
-        this.stats.add(Objects.requireNonNull(stat));
+        final MetricConfig statConfig = config == null ? this.config : config;
+        stats.add(new StatAndConfig(Objects.requireNonNull(stat), statConfig));
         Object lock = metricLock();
         for (NamedMeasurable m : stat.stats()) {
-            final KafkaMetric metric = new KafkaMetric(lock, m.name(), m.stat(), config == null ? this.config : config, time);
+            final KafkaMetric metric = new KafkaMetric(lock, m.name(), m.stat(), statConfig, time);
             if (!metrics.containsKey(metric.metricName())) {
                 registry.registerMetric(metric);
                 metrics.put(metric.metricName(), metric);
@@ -287,16 +306,17 @@ public final class Sensor {
         } else if (metrics.containsKey(metricName)) {
             return true;
         } else {
+            final MetricConfig statConfig = config == null ? this.config : config;
             final KafkaMetric metric = new KafkaMetric(
                 metricLock(),
                 Objects.requireNonNull(metricName),
                 Objects.requireNonNull(stat),
-                config == null ? this.config : config,
+                statConfig,
                 time
             );
             registry.registerMetric(metric);
             metrics.put(metric.metricName(), metric);
-            stats.add(stat);
+            stats.add(new StatAndConfig(Objects.requireNonNull(stat), statConfig));
             return true;
         }
     }

core/src/main/scala/kafka/server/ClientQuotaManager.scala
@@ -394,21 +394,25 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
       sensorAccessor.getOrCreate(
         getQuotaSensorName(metricTags),
         ClientQuotaManager.InactiveSensorExpirationTimeSeconds,
-        clientRateMetricName(metricTags),
-        Some(getQuotaMetricConfig(metricTags)),
-        new Rate
+        registerQuotaMetrics(metricTags)
       ),
       sensorAccessor.getOrCreate(
         getThrottleTimeSensorName(metricTags),
         ClientQuotaManager.InactiveSensorExpirationTimeSeconds,
-        throttleMetricName(metricTags),
-        None,
-        new Avg
+        sensor => sensor.add(throttleMetricName(metricTags), new Avg)
       )
     )
     if (quotaCallback.quotaResetRequired(clientQuotaType))
       updateQuotaMetricConfigs()
     sensors
+  }
+
+  protected def registerQuotaMetrics(metricTags: Map[String, String])(sensor: Sensor): Unit = {
+    sensor.add(
+      clientRateMetricName(metricTags),
+      new Rate,
+      getQuotaMetricConfig(metricTags)
+    )
   }
 
   private def metricTagsToSensorSuffix(metricTags: Map[String, String]): String =
@@ -420,7 +424,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
   private def getQuotaSensorName(metricTags: Map[String, String]): String =
     s"$quotaType-${metricTagsToSensorSuffix(metricTags)}"
 
-  private def getQuotaMetricConfig(metricTags: Map[String, String]): MetricConfig = {
+  protected def getQuotaMetricConfig(metricTags: Map[String, String]): MetricConfig = {
     getQuotaMetricConfig(quotaLimit(metricTags.asJava))
   }
 
@@ -435,9 +439,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
     sensorAccessor.getOrCreate(
       sensorName,
       ClientQuotaManager.InactiveSensorExpirationTimeSeconds,
-      metricName,
-      None,
-      new Rate
+      sensor => sensor.add(metricName, new Rate)
     )
   }
 

core/src/main/scala/kafka/server/ControllerMutationQuotaManager.scala
@@ -23,6 +23,8 @@ import org.apache.kafka.common.errors.ThrottlingQuotaExceededException
 import org.apache.kafka.common.metrics.Metrics
 import org.apache.kafka.common.metrics.QuotaViolationException
 import org.apache.kafka.common.metrics.Sensor
+import org.apache.kafka.common.metrics.stats.Rate
+import org.apache.kafka.common.metrics.stats.TokenBucket
 import org.apache.kafka.common.protocol.Errors
 import org.apache.kafka.common.utils.Time
 import org.apache.kafka.server.quota.ClientQuotaCallback
@@ -60,7 +62,7 @@ abstract class AbstractControllerMutationQuota(private val time: Time) extends C
   protected var lastRecordedTimeMs = 0L
 
   protected def updateThrottleTime(e: QuotaViolationException, timeMs: Long): Unit = {
-    lastThrottleTimeMs = ClientQuotaManager.throttleTime(e, timeMs)
+    lastThrottleTimeMs = ControllerMutationQuotaManager.throttleTimeMs(e, timeMs)
     lastRecordedTimeMs = timeMs
   }
 
@@ -131,6 +133,22 @@ class PermissiveControllerMutationQuota(private val time: Time,
 
 object ControllerMutationQuotaManager {
   val QuotaControllerMutationDefault = Int.MaxValue.toDouble
+
+  /**
+   * This calculates the amount of time needed to bring the TokenBucket within quota
+   * assuming that no new metrics are recorded.
+   *
+   * Basically, if a value < 0 is observed, the time required to bring it to zero is
+   * -value / refill rate (quota bound) * 1000.
+   */
+  def throttleTimeMs(e: QuotaViolationException, timeMs: Long): Long = {
+    e.metric().measurable() match {
+      case _: TokenBucket =>
+        Math.round(-e.value() / e.bound() * 1000)
+      case _ => throw new IllegalArgumentException(
+        s"Metric ${e.metric().metricName()} is not a TokenBucket metric, value ${e.metric().measurable()}")
+    }
+  }
 }
 
 /**
@@ -156,6 +174,24 @@ class ControllerMutationQuotaManager(private val config: ClientQuotaManagerConfi
       quotaMetricTags.asJava)
   }
 
+  private def clientTokenBucketMetricName(quotaMetricTags: Map[String, String]): MetricName = {
+    metrics.metricName("tokens", QuotaType.ControllerMutation.toString,
+      "Tracking remaining tokens in the token bucket per user/client-id",
+      quotaMetricTags.asJava)
+  }
+
+  override protected def registerQuotaMetrics(metricTags: Map[String, String])(sensor: Sensor): Unit = {
+    sensor.add(
+      clientRateMetricName(metricTags),
+      new Rate
+    )
+    sensor.add(
+      clientTokenBucketMetricName(metricTags),
+      new TokenBucket,
+      getQuotaMetricConfig(metricTags)
+    )
+  }
+
   /**
    * Records that a user/clientId accumulated or would like to accumulate the provided amount at the
    * the specified time, returns throttle time in milliseconds. The quota is strict meaning that it
@@ -179,7 +215,7 @@ class ControllerMutationQuotaManager(private val config: ClientQuotaManagerConfi
       0
     } catch {
       case e: QuotaViolationException =>
-        val throttleTimeMs = throttleTime(e, timeMs).toInt
+        val throttleTimeMs = ControllerMutationQuotaManager.throttleTimeMs(e, timeMs).toInt
         debug(s"Quota violated for sensor (${quotaSensor.name}). Delay time: ($throttleTimeMs)")
         throttleTimeMs
     }

clients/src/test/java/org/apache/kafka/common/metrics/SensorTest.java
@@ -21,6 +21,7 @@ import org.apache.kafka.common.metrics.stats.Avg;
 import org.apache.kafka.common.metrics.stats.CumulativeCount;
 import org.apache.kafka.common.metrics.stats.Meter;
 import org.apache.kafka.common.metrics.stats.Rate;
+import org.apache.kafka.common.metrics.stats.TokenBucket;
 import org.apache.kafka.common.metrics.stats.WindowedSum;
 import org.apache.kafka.common.utils.MockTime;
 import org.apache.kafka.common.utils.SystemTime;
@@ -39,12 +40,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import org.mockito.Mockito;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -208,5 +211,96 @@ public class SensorTest {
         );
 
         assertThat(sensor.hasMetrics(), is(true));
+    }
+
+    @Test
+    public void testStrictQuotaEnforcementWithRate() {
+        final Time time = new MockTime(0, System.currentTimeMillis(), 0);
+        final Metrics metrics = new Metrics(time);
+        final Sensor sensor = metrics.sensor("sensor", new MetricConfig()
+            .quota(Quota.upperBound(2))
+            .timeWindow(1, TimeUnit.SECONDS)
+            .samples(11));
+        final MetricName metricName = metrics.metricName("rate", "test-group");
+        assertTrue(sensor.add(metricName, new Rate()));
+        final KafkaMetric rateMetric = metrics.metric(metricName);
+
+        // Recording a first value at T+0 to bring the avg rate to 3 which is already
+        // above the quota.
+        strictRecord(sensor, 30, time.milliseconds());
+        assertEquals(3, rateMetric.measurableValue(time.milliseconds()), 0.1);
+
+        // Theoretically, we should wait 5s to bring back the avg rate to the define quota:
+        // ((30 / 10) - 2) / 2 * 10 = 5s
+        time.sleep(5000);
+
+        // But, recording a second value is rejected because the avg rate is still equal
+        // to 3 after 5s.
+        assertEquals(3, rateMetric.measurableValue(time.milliseconds()), 0.1);
+        assertThrows(QuotaViolationException.class, () -> strictRecord(sensor, 30, time.milliseconds()));
+
+        metrics.close();
+    }
+
+    @Test
+    public void testStrictQuotaEnforcementWithTokenBucket() {
+        final Time time = new MockTime(0, System.currentTimeMillis(), 0);
+        final Metrics metrics = new Metrics(time);
+        final Sensor sensor = metrics.sensor("sensor", new MetricConfig()
+            .quota(Quota.upperBound(2))
+            .timeWindow(1, TimeUnit.SECONDS)
+            .samples(10));
+        final MetricName metricName = metrics.metricName("credits", "test-group");
+        assertTrue(sensor.add(metricName, new TokenBucket()));
+        final KafkaMetric tkMetric = metrics.metric(metricName);
+
+        // Recording a first value at T+0 to bring the remaining credits below zero
+        strictRecord(sensor, 30, time.milliseconds());
+        assertEquals(-10, tkMetric.measurableValue(time.milliseconds()), 0.1);
+
+        // Theoretically, we should wait 5s to bring back the avg rate to the define quota:
+        // 10 / 2 = 5s
+        time.sleep(5000);
+
+        // Unlike the default rate based on a windowed sum, it works as expected.
+        assertEquals(0, tkMetric.measurableValue(time.milliseconds()), 0.1);
+        strictRecord(sensor, 30, time.milliseconds());
+        assertEquals(-30, tkMetric.measurableValue(time.milliseconds()), 0.1);
+
+        metrics.close();
+    }
+
+    private void strictRecord(Sensor sensor, double value, long timeMs) {
+        synchronized (sensor) {
+            sensor.checkQuotas(timeMs);
+            sensor.record(value, timeMs, false);
+        }
+    }
+
+    @Test
+    public void testRecordAndCheckQuotaUseMetricConfigOfEachStat() {
+        final Time time = new MockTime(0, System.currentTimeMillis(), 0);
+        final Metrics metrics = new Metrics(time);
+        final Sensor sensor = metrics.sensor("sensor");
+
+        final MeasurableStat stat1 = Mockito.mock(MeasurableStat.class);
+        final MetricName stat1Name = metrics.metricName("stat1", "test-group");
+        final MetricConfig stat1Config = new MetricConfig().quota(Quota.upperBound(5));
+        sensor.add(stat1Name, stat1, stat1Config);
+
+        final MeasurableStat stat2 = Mockito.mock(MeasurableStat.class);
+        final MetricName stat2Name = metrics.metricName("stat2", "test-group");
+        final MetricConfig stat2Config = new MetricConfig().quota(Quota.upperBound(10));
+        sensor.add(stat2Name, stat2, stat2Config);
+
+        sensor.record(10, 1);
+        Mockito.verify(stat1).record(stat1Config, 10, 1);
+        Mockito.verify(stat2).record(stat2Config, 10, 1);
+
+        Mockito.when(stat1.measure(stat1Config, 2)).thenReturn(2.0);
+        Mockito.when(stat2.measure(stat2Config, 2)).thenReturn(2.0);
+        sensor.checkQuotas(2);
+
+        metrics.close();
     }
 }

commit 22463aab1e57289db6dcc02692299e164db71902

KAFKA-10458; Updating controller quota does not work since Token Bucket (#9272)
This PR fixes two issues that have been introduced by #9114.
- When the metric was switched from Rate to TokenBucket in the ControllerMutationQuotaManager, the metrics were mixed up. That broke the quota update path.
- When a quota is updated, the ClientQuotaManager updates the MetricConfig of the KafkaMetric. That update was not reflected into the Sensor so the Sensor was still using the MetricConfig that it has been created with.

Reviewers: Anna Povzner <anna@confluent.io>, Rajini Sivaram <rajinisivaram@googlemail.com>

clients/src/main/java/org/apache/kafka/common/metrics/Sensor.java
@@ -16,6 +16,7 @@
  */
 package org.apache.kafka.common.metrics;
 
+import java.util.function.Supplier;
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.metrics.CompoundStat.NamedMeasurable;
 import org.apache.kafka.common.metrics.stats.TokenBucket;
@@ -53,12 +54,20 @@ public final class Sensor {
     private final Object metricLock;
 
     private static class StatAndConfig {
-        public final Stat stat;
-        public final MetricConfig config;
+        private final Stat stat;
+        private final Supplier<MetricConfig> configSupplier;
 
-        StatAndConfig(Stat stat, MetricConfig config) {
+        StatAndConfig(Stat stat, Supplier<MetricConfig> configSupplier) {
             this.stat = stat;
-            this.config = config;
+            this.configSupplier = configSupplier;
+        }
+
+        public Stat stat() {
+            return stat;
+        }
+
+        public MetricConfig config() {
+            return configSupplier.get();
         }
     }
 
@@ -219,7 +228,7 @@ public final class Sensor {
             synchronized (metricLock()) {
                 // increment all the stats
                 for (StatAndConfig statAndConfig : this.stats) {
-                    statAndConfig.stat.record(statAndConfig.config, value, timeMs);
+                    statAndConfig.stat.record(statAndConfig.config(), value, timeMs);
                 }
             }
             if (checkQuotas)
@@ -278,7 +287,7 @@ public final class Sensor {
             return false;
 
         final MetricConfig statConfig = config == null ? this.config : config;
-        stats.add(new StatAndConfig(Objects.requireNonNull(stat), statConfig));
+        stats.add(new StatAndConfig(Objects.requireNonNull(stat), () -> statConfig));
         Object lock = metricLock();
         for (NamedMeasurable m : stat.stats()) {
             final KafkaMetric metric = new KafkaMetric(lock, m.name(), m.stat(), statConfig, time);
@@ -324,7 +333,7 @@ public final class Sensor {
             );
             registry.registerMetric(metric);
             metrics.put(metric.metricName(), metric);
-            stats.add(new StatAndConfig(Objects.requireNonNull(stat), statConfig));
+            stats.add(new StatAndConfig(Objects.requireNonNull(stat), metric::config));
             return true;
         }
     }

core/src/main/scala/kafka/server/ClientQuotaManager.scala
@@ -409,7 +409,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
 
   protected def registerQuotaMetrics(metricTags: Map[String, String])(sensor: Sensor): Unit = {
     sensor.add(
-      clientRateMetricName(metricTags),
+      clientQuotaMetricName(metricTags),
       new Rate,
       getQuotaMetricConfig(metricTags)
     )
@@ -522,7 +522,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
       val clientId = quotaEntity.clientId
       val metricTags = Map(DefaultTags.User -> user, DefaultTags.ClientId -> clientId)
 
-      val quotaMetricName = clientRateMetricName(metricTags)
+      val quotaMetricName = clientQuotaMetricName(metricTags)
       // Change the underlying metric config if the sensor has been created
       val metric = allMetrics.get(quotaMetricName)
       if (metric != null) {
@@ -532,7 +532,7 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
         }
       }
     } else {
-      val quotaMetricName = clientRateMetricName(Map.empty)
+      val quotaMetricName = clientQuotaMetricName(Map.empty)
       allMetrics.forEach { (metricName, metric) =>
         if (metricName.name == quotaMetricName.name && metricName.group == quotaMetricName.group) {
           val metricTags = metricName.tags
@@ -547,7 +547,11 @@ class ClientQuotaManager(private val config: ClientQuotaManagerConfig,
     }
   }
 
-  protected def clientRateMetricName(quotaMetricTags: Map[String, String]): MetricName = {
+  /**
+   * Returns the MetricName of the metric used for the quota. The name is used to create the
+   * metric but also to find the metric when the quota is changed.
+   */
+  protected def clientQuotaMetricName(quotaMetricTags: Map[String, String]): MetricName = {
     metrics.metricName("byte-rate", quotaType.toString,
       "Tracking byte-rate per user/client-id",
       quotaMetricTags.asJava)

core/src/main/scala/kafka/server/ClientRequestQuotaManager.scala
@@ -79,7 +79,7 @@ class ClientRequestQuotaManager(private val config: ClientQuotaManagerConfig,
     QuotaUtils.boundedThrottleTime(e, maxThrottleTimeMs, timeMs)
   }
 
-  override protected def clientRateMetricName(quotaMetricTags: Map[String, String]): MetricName = {
+  override protected def clientQuotaMetricName(quotaMetricTags: Map[String, String]): MetricName = {
     metrics.metricName("request-time", QuotaType.Request.toString,
       "Tracking request-time per user/client-id",
       quotaMetricTags.asJava)

core/src/main/scala/kafka/server/ControllerMutationQuotaManager.scala
@@ -168,15 +168,15 @@ class ControllerMutationQuotaManager(private val config: ClientQuotaManagerConfi
                                      private val quotaCallback: Option[ClientQuotaCallback])
     extends ClientQuotaManager(config, metrics, QuotaType.ControllerMutation, time, threadNamePrefix, quotaCallback) {
 
-  override protected def clientRateMetricName(quotaMetricTags: Map[String, String]): MetricName = {
-    metrics.metricName("mutation-rate", QuotaType.ControllerMutation.toString,
-      "Tracking mutation-rate per user/client-id",
+  override protected def clientQuotaMetricName(quotaMetricTags: Map[String, String]): MetricName = {
+    metrics.metricName("tokens", QuotaType.ControllerMutation.toString,
+      "Tracking remaining tokens in the token bucket per user/client-id",
       quotaMetricTags.asJava)
   }
 
-  private def clientTokenBucketMetricName(quotaMetricTags: Map[String, String]): MetricName = {
-    metrics.metricName("tokens", QuotaType.ControllerMutation.toString,
-      "Tracking remaining tokens in the token bucket per user/client-id",
+  private def clientRateMetricName(quotaMetricTags: Map[String, String]): MetricName = {
+    metrics.metricName("mutation-rate", QuotaType.ControllerMutation.toString,
+      "Tracking mutation-rate per user/client-id",
       quotaMetricTags.asJava)
   }
 
@@ -186,7 +186,7 @@ class ControllerMutationQuotaManager(private val config: ClientQuotaManagerConfi
       new Rate
     )
     sensor.add(
-      clientTokenBucketMetricName(metricTags),
+      clientQuotaMetricName(metricTags),
       new TokenBucket,
       getQuotaMetricConfig(metricTags)
     )

clients/src/test/java/org/apache/kafka/common/metrics/SensorTest.java
@@ -336,9 +336,39 @@ public class SensorTest {
         Mockito.verify(stat1).record(stat1Config, 10, 1);
         Mockito.verify(stat2).record(stat2Config, 10, 1);
 
-        Mockito.when(stat1.measure(stat1Config, 2)).thenReturn(2.0);
-        Mockito.when(stat2.measure(stat2Config, 2)).thenReturn(2.0);
         sensor.checkQuotas(2);
+        Mockito.verify(stat1).measure(stat1Config, 2);
+        Mockito.verify(stat2).measure(stat2Config, 2);
+
+        metrics.close();
+    }
+
+    @Test
+    public void testUpdatingMetricConfigIsReflectedInTheSensor() {
+        final Time time = new MockTime(0, System.currentTimeMillis(), 0);
+        final Metrics metrics = new Metrics(time);
+        final Sensor sensor = metrics.sensor("sensor");
+
+        final MeasurableStat stat = Mockito.mock(MeasurableStat.class);
+        final MetricName statName = metrics.metricName("stat", "test-group");
+        final MetricConfig statConfig = new MetricConfig().quota(Quota.upperBound(5));
+        sensor.add(statName, stat, statConfig);
+
+        sensor.record(10, 1);
+        Mockito.verify(stat).record(statConfig, 10, 1);
+
+        sensor.checkQuotas(2);
+        Mockito.verify(stat).measure(statConfig, 2);
+
+        // Update the config of the KafkaMetric
+        final MetricConfig newConfig = new MetricConfig().quota(Quota.upperBound(10));
+        metrics.metric(statName).config(newConfig);
+
+        sensor.record(10, 3);
+        Mockito.verify(stat).record(newConfig, 10, 3);
+
+        sensor.checkQuotas(4);
+        Mockito.verify(stat).measure(newConfig, 4);
 
         metrics.close();
     }

core/src/test/scala/unit/kafka/server/ControllerMutationQuotaTest.scala
@@ -17,6 +17,7 @@ import java.util.Properties
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 
+import kafka.server.ClientQuotaManager.DefaultTags
 import kafka.utils.TestUtils
 import org.apache.kafka.common.internals.KafkaFutureImpl
 import org.apache.kafka.common.message.CreatePartitionsRequestData
@@ -24,6 +25,7 @@ import org.apache.kafka.common.message.CreatePartitionsRequestData.CreatePartiti
 import org.apache.kafka.common.message.CreateTopicsRequestData
 import org.apache.kafka.common.message.CreateTopicsRequestData.CreatableTopic
 import org.apache.kafka.common.message.DeleteTopicsRequestData
+import org.apache.kafka.common.metrics.KafkaMetric
 import org.apache.kafka.common.protocol.ApiKeys
 import org.apache.kafka.common.protocol.Errors
 import org.apache.kafka.common.quota.ClientQuotaAlteration
@@ -41,6 +43,7 @@ import org.apache.kafka.common.security.auth.KafkaPrincipal
 import org.apache.kafka.common.security.auth.KafkaPrincipalBuilder
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Test
 
@@ -80,6 +83,8 @@ object ControllerMutationQuotaTest {
   val TopicsWith30Partitions = Map(Topic1 -> 30, Topic2 -> 30)
   val TopicsWith31Partitions = Map(Topic1 -> 31, Topic2 -> 31)
 
+  val ControllerQuotaSamples = 10
+  val ControllerQuotaWindowSizeSeconds = 1
   val ControllerMutationRate = 2.0
 }
 
@@ -95,8 +100,8 @@ class ControllerMutationQuotaTest extends BaseRequestTest {
     properties.put(KafkaConfig.PrincipalBuilderClassProp,
       classOf[ControllerMutationQuotaTest.TestPrincipalBuilder].getName)
     // Specify number of samples and window size.
-    properties.put(KafkaConfig.NumControllerQuotaSamplesProp, "10")
-    properties.put(KafkaConfig.ControllerQuotaWindowSizeSecondsProp, "1")
+    properties.put(KafkaConfig.NumControllerQuotaSamplesProp, ControllerQuotaSamples.toString)
+    properties.put(KafkaConfig.ControllerQuotaWindowSizeSecondsProp, ControllerQuotaWindowSizeSeconds.toString)
   }
 
   @Before
@@ -122,6 +127,28 @@ class ControllerMutationQuotaTest extends BaseRequestTest {
     defineUserQuota(principal.getName, None)
     // Back to the default
     waitUserQuota(principal.getName, Long.MaxValue)
+  }
+
+  @Test
+  def testQuotaMetric(): Unit = {
+    asPrincipal(ThrottledPrincipal) {
+      // Metric is lazily created
+      assertTrue(quotaMetric(principal.getName).isEmpty)
+
+      // Create a topic to create the metrics
+      val (_, errors) = createTopics(Map("topic" -> 1), StrictDeleteTopicsRequestVersion)
+      assertEquals(Set(Errors.NONE), errors.values.toSet)
+
+      // Metric must be there with the correct config
+      verifyQuotaMetric(principal.getName, ControllerMutationRate)
+
+      // Update quota
+      defineUserQuota(ThrottledPrincipal.getName, Some(ControllerMutationRate * 2))
+      waitUserQuota(ThrottledPrincipal.getName, ControllerMutationRate * 2)
+
+      // Metric must be there with the updated config
+      verifyQuotaMetric(principal.getName, ControllerMutationRate * 2)
+    }
   }
 
   @Test
@@ -342,6 +369,29 @@ class ControllerMutationQuotaTest extends BaseRequestTest {
       actualQuota = quotaManager.quota(user, "").bound()
       expectedQuota == actualQuota
     }, s"Quota of $user is not $expectedQuota but $actualQuota")
+  }
+
+  private def quotaMetric(user: String): Option[KafkaMetric] = {
+    val metrics = servers.head.metrics
+    val metricName = metrics.metricName(
+      "tokens",
+      QuotaType.ControllerMutation.toString,
+      "Tracking remaining tokens in the token bucket per user/client-id",
+      Map(DefaultTags.User -> user, DefaultTags.ClientId -> "").asJava)
+    Option(servers.head.metrics.metric(metricName))
+  }
+
+  private def verifyQuotaMetric(user: String, expectedQuota: Double): Unit = {
+    quotaMetric(user) match {
+      case Some(metric) =>
+        val config = metric.config()
+        assertEquals(expectedQuota, config.quota().bound(), 0.1)
+        assertEquals(ControllerQuotaSamples, config.samples())
+        assertEquals(ControllerQuotaWindowSizeSeconds * 1000, config.timeWindowMs())
+
+      case None =>
+        fail(s"Quota metric of $user is not defined")
+    }
   }
 
   private def alterClientQuotas(request: Map[ClientQuotaEntity, Map[String, Option[Double]]]): Map[ClientQuotaEntity, KafkaFutureImpl[Void]] = {