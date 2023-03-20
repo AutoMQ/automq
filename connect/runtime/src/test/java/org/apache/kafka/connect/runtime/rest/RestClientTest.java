AutoMQ/automq

commit ff36fcf7c259ddab7c2011b596d488cbdc36b832

KAFKA-14346: Remove hard-to-mock RestClient calls (#12828)
Reviewers: Chris Egerton <chrise@aiven.io>

connect/runtime/src/main/java/org/apache/kafka/connect/runtime/rest/RestClient.java
@@ -43,9 +43,23 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Client for outbound REST requests to other members of a Connect cluster
+ * This class is thread-safe.
+ */
 public class RestClient {
     private static final Logger log = LoggerFactory.getLogger(RestClient.class);
     private static final ObjectMapper JSON_SERDE = new ObjectMapper();
+    private WorkerConfig config;
+
+    public RestClient(WorkerConfig config) {
+        this.config = config;
+    }
+
+    // VisibleForTesting
+    HttpClient httpClient() {
+        return new HttpClient(SSLUtils.createClientSideSslContextFactory(config));
+    }
 
     /**
      * Sends HTTP request to remote REST server
@@ -58,9 +72,9 @@ public class RestClient {
      * @param <T>             The type of the deserialized response to the HTTP request.
      * @return The deserialized response to the HTTP request, or null if no data is expected.
      */
-    public static <T> HttpResponse<T> httpRequest(String url, String method, HttpHeaders headers, Object requestBodyData,
-                                                  TypeReference<T> responseFormat, WorkerConfig config) {
-        return httpRequest(url, method, headers, requestBodyData, responseFormat, config, null, null);
+    public <T> HttpResponse<T> httpRequest(String url, String method, HttpHeaders headers, Object requestBodyData,
+                                                  TypeReference<T> responseFormat) {
+        return httpRequest(url, method, headers, requestBodyData, responseFormat, null, null);
     }
 
     /**
@@ -78,17 +92,10 @@ public class RestClient {
      *                                  may be null if the request doesn't need to be signed
      * @return The deserialized response to the HTTP request, or null if no data is expected.
      */
-    public static <T> HttpResponse<T> httpRequest(String url, String method, HttpHeaders headers, Object requestBodyData,
-                                                  TypeReference<T> responseFormat, WorkerConfig config,
+    public <T> HttpResponse<T> httpRequest(String url, String method, HttpHeaders headers, Object requestBodyData,
+                                                  TypeReference<T> responseFormat,
                                                   SecretKey sessionKey, String requestSignatureAlgorithm) {
-        HttpClient client;
-
-        if (url.startsWith("https://")) {
-            client = new HttpClient(SSLUtils.createClientSideSslContextFactory(config));
-        } else {
-            client = new HttpClient();
-        }
-
+        HttpClient client = httpClient();
         client.setFollowRedirects(false);
 
         try {
@@ -109,7 +116,7 @@ public class RestClient {
         }
     }
 
-    static <T> HttpResponse<T> httpRequest(HttpClient client, String url, String method,
+    private <T> HttpResponse<T> httpRequest(HttpClient client, String url, String method,
                                            HttpHeaders headers, Object requestBodyData,
                                            TypeReference<T> responseFormat, SecretKey sessionKey,
                                            String requestSignatureAlgorithm) {

connect/runtime/src/test/java/org/apache/kafka/connect/runtime/rest/RestClientTest.java
@@ -50,7 +50,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 @RunWith(Enclosed.class)
@@ -74,18 +76,20 @@ public class RestClientTest {
     }
 
     private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient, String requestSignatureAlgorithm) {
-        return RestClient.httpRequest(
-                httpClient,
-                "https://localhost:1234/api/endpoint",
-                "GET",
-                null,
-                new TestDTO("requestBodyData"),
-                TEST_TYPE,
-                MOCK_SECRET_KEY,
-                requestSignatureAlgorithm);
+        RestClient client = spy(new RestClient(null));
+        doReturn(httpClient).when(client).httpClient();
+        return client.httpRequest(
+                        "https://localhost:1234/api/endpoint",
+                        "GET",
+                        null,
+                        new TestDTO("requestBodyData"),
+                        TEST_TYPE,
+                        MOCK_SECRET_KEY,
+                        requestSignatureAlgorithm
+                );
     }
 
-    private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient) {
+    private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient) throws Exception {
         String validRequestSignatureAlgorithm = "HmacSHA1";
         return httpRequest(httpClient, validRequestSignatureAlgorithm);
     }

build.gradle
@@ -2627,6 +2627,7 @@ project(':connect:runtime') {
     testImplementation libs.httpclient
 
     testRuntimeOnly libs.slf4jlog4j
+    testRuntimeOnly libs.bcpkix
   }
 
   javadoc {

checkstyle/import-control.xml
@@ -630,6 +630,8 @@
       <allow pkg="org.apache.kafka.connect" />
       <allow pkg="org.apache.kafka.tools" />
       <allow pkg="javax.ws.rs" />
+      <allow pkg="org.apache.http"/>
+      <allow pkg="org.eclipse.jetty.util"/>
     </subpackage>
 
     <subpackage name="json">

connect/mirror/src/main/java/org/apache/kafka/connect/mirror/MirrorMaker.java
@@ -255,9 +255,10 @@ public class MirrorMaker {
         // Pass the shared admin to the distributed herder as an additional AutoCloseable object that should be closed when the
         // herder is stopped. MirrorMaker has multiple herders, and having the herder own the close responsibility is much easier than
         // tracking the various shared admin objects in this class.
+        // Do not provide a restClient to the DistributedHerder to indicate that request forwarding is disabled
         Herder herder = new DistributedHerder(distributedConfig, time, worker,
                 kafkaClusterId, statusBackingStore, configBackingStore,
-                advertisedUrl, CLIENT_CONFIG_OVERRIDE_POLICY, sharedAdmin);
+                advertisedUrl, null, CLIENT_CONFIG_OVERRIDE_POLICY, sharedAdmin);
         herders.put(sourceAndTarget, herder);
     }
 

connect/runtime/src/main/java/org/apache/kafka/connect/cli/ConnectDistributed.java
@@ -28,6 +28,7 @@ import org.apache.kafka.connect.runtime.WorkerInfo;
 import org.apache.kafka.connect.runtime.distributed.DistributedConfig;
 import org.apache.kafka.connect.runtime.distributed.DistributedHerder;
 import org.apache.kafka.connect.runtime.isolation.Plugins;
+import org.apache.kafka.connect.runtime.rest.RestClient;
 import org.apache.kafka.connect.runtime.rest.RestServer;
 import org.apache.kafka.connect.storage.ConfigBackingStore;
 import org.apache.kafka.connect.storage.Converter;
@@ -99,7 +100,9 @@ public class ConnectDistributed {
         String kafkaClusterId = config.kafkaClusterId();
         log.debug("Kafka cluster ID: {}", kafkaClusterId);
 
-        RestServer rest = new RestServer(config);
+        RestClient restClient = new RestClient(config);
+
+        RestServer rest = new RestServer(config, restClient);
         rest.initializeServer();
 
         URI advertisedUrl = rest.advertisedUrl();
@@ -138,7 +141,7 @@ public class ConnectDistributed {
         // herder is stopped. This is easier than having to track and own the lifecycle ourselves.
         DistributedHerder herder = new DistributedHerder(config, time, worker,
                 kafkaClusterId, statusBackingStore, configBackingStore,
-                advertisedUrl.toString(), connectorClientConfigOverridePolicy, sharedAdmin);
+                advertisedUrl.toString(), restClient, connectorClientConfigOverridePolicy, sharedAdmin);
 
         final Connect connect = new Connect(herder, rest);
         log.info("Kafka Connect distributed worker initialization took {}ms", time.hiResClockMs() - initStart);

connect/runtime/src/main/java/org/apache/kafka/connect/cli/ConnectStandalone.java
@@ -82,7 +82,8 @@ public class ConnectStandalone {
             String kafkaClusterId = config.kafkaClusterId();
             log.debug("Kafka cluster ID: {}", kafkaClusterId);
 
-            RestServer rest = new RestServer(config);
+            // Do not initialize a RestClient because the ConnectorsResource will not use it in standalone mode.
+            RestServer rest = new RestServer(config, null);
             rest.initializeServer();
 
             URI advertisedUrl = rest.advertisedUrl();

connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/DistributedHerder.java
@@ -166,6 +166,7 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
     private final String requestSignatureAlgorithm;
     private final List<String> keySignatureVerificationAlgorithms;
     private final KeyGenerator keyGenerator;
+    private final RestClient restClient;
 
     // Visible for testing
     ExecutorService forwardRequestExecutor;
@@ -240,9 +241,11 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
                              StatusBackingStore statusBackingStore,
                              ConfigBackingStore configBackingStore,
                              String restUrl,
+                             RestClient restClient,
                              ConnectorClientConfigOverridePolicy connectorClientConfigOverridePolicy,
                              AutoCloseable... uponShutdown) {
-        this(config, worker, worker.workerId(), kafkaClusterId, statusBackingStore, configBackingStore, null, restUrl, worker.metrics(),
+        this(config, worker, worker.workerId(), kafkaClusterId, statusBackingStore, configBackingStore,
+             null, restUrl, restClient, worker.metrics(),
              time, connectorClientConfigOverridePolicy, uponShutdown);
         configBackingStore.setUpdateListener(new ConfigUpdateListener());
     }
@@ -256,6 +259,7 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
                       ConfigBackingStore configBackingStore,
                       WorkerGroupMember member,
                       String restUrl,
+                      RestClient restClient,
                       ConnectMetrics metrics,
                       Time time,
                       ConnectorClientConfigOverridePolicy connectorClientConfigOverridePolicy,
@@ -272,6 +276,7 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
         this.keyRotationIntervalMs = config.getInt(DistributedConfig.INTER_WORKER_KEY_TTL_MS_CONFIG);
         this.keySignatureVerificationAlgorithms = config.getList(DistributedConfig.INTER_WORKER_VERIFICATION_ALGORITHMS_CONFIG);
         this.keyGenerator = config.getInternalRequestKeyGenerator();
+        this.restClient = restClient;
         this.isTopicTrackingEnabled = config.getBoolean(TOPIC_TRACKING_ENABLE_CONFIG);
         this.uponShutdown = Arrays.asList(uponShutdown);
 
@@ -1157,16 +1162,30 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
             if (error == null) {
                 callback.onCompletion(null, null);
             } else if (error instanceof NotLeaderException) {
-                String forwardedUrl = ((NotLeaderException) error).forwardUrl() + "connectors/" + id.connector() + "/fence";
-                log.trace("Forwarding zombie fencing request for connector {} to leader at {}", id.connector(), forwardedUrl);
-                forwardRequestExecutor.execute(() -> {
-                    try {
-                        RestClient.httpRequest(forwardedUrl, "PUT", null, null, null, config, sessionKey, requestSignatureAlgorithm);
-                        callback.onCompletion(null, null);
-                    } catch (Throwable t) {
-                        callback.onCompletion(t, null);
-                    }
-                });
+                if (restClient != null) {
+                    String forwardedUrl = ((NotLeaderException) error).forwardUrl() + "connectors/" + id.connector() + "/fence";
+                    log.trace("Forwarding zombie fencing request for connector {} to leader at {}", id.connector(), forwardedUrl);
+                    forwardRequestExecutor.execute(() -> {
+                        try {
+                            restClient.httpRequest(forwardedUrl, "PUT", null, null, null, sessionKey, requestSignatureAlgorithm);
+                            callback.onCompletion(null, null);
+                        } catch (Throwable t) {
+                            callback.onCompletion(t, null);
+                        }
+                    });
+                } else {
+                    callback.onCompletion(
+                            new ConnectException(
+                                    // TODO: Update this message if KIP-710 is accepted and merged
+                                    //       (https://cwiki.apache.org/confluence/display/KAFKA/KIP-710%3A+Full+support+for+distributed+mode+in+dedicated+MirrorMaker+2.0+clusters)
+                                    "This worker is not able to communicate with the leader of the cluster, "
+                                            + "which is required for exactly-once source tasks. If running MirrorMaker 2 "
+                                            + "in dedicated mode, consider either disabling exactly-once support, or deploying "
+                                            + "the connectors for MirrorMaker 2 directly onto a distributed Kafka Connect cluster."
+                            ),
+                            null
+                    );
+                }
             } else {
                 error = ConnectUtils.maybeWrap(error, "Failed to perform zombie fencing");
                 callback.onCompletion(error, null);
@@ -1906,6 +1925,15 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
                 if (isLeader()) {
                     writeToConfigTopicAsLeader(() -> configBackingStore.putTaskConfigs(connName, rawTaskProps));
                     cb.onCompletion(null, null);
+                } else if (restClient == null) {
+                    // TODO: Update this message if KIP-710 is accepted and merged
+                    //       (https://cwiki.apache.org/confluence/display/KAFKA/KIP-710%3A+Full+support+for+distributed+mode+in+dedicated+MirrorMaker+2.0+clusters)
+                    throw new NotLeaderException("This worker is not able to communicate with the leader of the cluster, "
+                            + "which is required for dynamically-reconfiguring connectors. If running MirrorMaker 2 "
+                            + "in dedicated mode, consider deploying the connectors for MirrorMaker 2 directly onto a "
+                            + "distributed Kafka Connect cluster.",
+                            leaderUrl()
+                    );
                 } else {
                     // We cannot forward the request on the same thread because this reconfiguration can happen as a result of connector
                     // addition or removal. If we blocked waiting for the response from leader, we may be kicked out of the worker group.
@@ -1925,7 +1953,7 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
                                     .build()
                                     .toString();
                             log.trace("Forwarding task configurations for connector {} to leader", connName);
-                            RestClient.httpRequest(reconfigUrl, "POST", null, rawTaskProps, null, config, sessionKey, requestSignatureAlgorithm);
+                            restClient.httpRequest(reconfigUrl, "POST", null, rawTaskProps, null, sessionKey, requestSignatureAlgorithm);
                             cb.onCompletion(null, null);
                         } catch (ConnectException e) {
                             log.error("Request to leader to reconfigure connector tasks failed", e);

commit a7f856f75213bc531a7653efaf1accf0d8307802

KAFKA-14816: Only load SSL properties when issuing cross-worker requests to HTTPS URLs (#13415)
This fixes a regression introduced in #12828, which caused workers to start unconditionally loading (and therefore validating) SSL-related properties when issuing REST requests to other workers. That was fine for the most part, but caused unnecessary failures when workers were configured with invalid SSL-related properties and their REST API used HTTP instead of HTTPS.

Reviewers: Ian McDonald <imcdonald@confluent.io>, Greg Harris <greg.harris@aiven.io>, Yash Mayya <yash.mayya@gmail.com>, Justine Olshan <jolshan@confluent.io>

connect/runtime/src/main/java/org/apache/kafka/connect/runtime/rest/RestClient.java
@@ -31,6 +31,7 @@ import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.http.HttpField;
 import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -59,8 +60,8 @@ public class RestClient {
     }
 
     // VisibleForTesting
-    HttpClient httpClient() {
-        return new HttpClient(SSLUtils.createClientSideSslContextFactory(config));
+    HttpClient httpClient(SslContextFactory sslContextFactory) {
+        return sslContextFactory != null ? new HttpClient(sslContextFactory) : new HttpClient();
     }
 
     /**
@@ -97,7 +98,11 @@ public class RestClient {
     public <T> HttpResponse<T> httpRequest(String url, String method, HttpHeaders headers, Object requestBodyData,
                                                   TypeReference<T> responseFormat,
                                                   SecretKey sessionKey, String requestSignatureAlgorithm) {
-        HttpClient client = httpClient();
+        // Only try to load SSL configs if we have to (see KAFKA-14816)
+        SslContextFactory sslContextFactory = url.startsWith("https://")
+                ? SSLUtils.createClientSideSslContextFactory(config)
+                : null;
+        HttpClient client = httpClient(sslContextFactory);
         client.setFollowRedirects(false);
 
         try {

connect/runtime/src/test/java/org/apache/kafka/connect/runtime/rest/RestClientTest.java
@@ -46,9 +46,11 @@ import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -75,18 +77,24 @@ public class RestClientTest {
         return mockKey;
     }
 
-    private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient, String requestSignatureAlgorithm) {
+    private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient, String requestSignatureAlgorithm, boolean https) {
         RestClient client = spy(new RestClient(null));
-        doReturn(httpClient).when(client).httpClient();
+        doReturn(httpClient).when(client).httpClient(any());
+        String protocol = https ? "https" : "http";
+        String url = protocol + "://localhost:1234/api/endpoint";
         return client.httpRequest(
-                        "https://localhost:1234/api/endpoint",
-                        "GET",
-                        null,
-                        new TestDTO("requestBodyData"),
-                        TEST_TYPE,
-                        MOCK_SECRET_KEY,
-                        requestSignatureAlgorithm
-                );
+                url,
+                "GET",
+                null,
+                new TestDTO("requestBodyData"),
+                TEST_TYPE,
+                MOCK_SECRET_KEY,
+                requestSignatureAlgorithm
+        );
+    }
+
+    private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient, String requestSignatureAlgorithm) {
+        return httpRequest(httpClient, requestSignatureAlgorithm, false);
     }
 
     private static RestClient.HttpResponse<TestDTO> httpRequest(HttpClient httpClient) throws Exception {
@@ -224,6 +232,19 @@ public class RestClientTest {
 
             ConnectRestException e = assertThrows(ConnectRestException.class, () -> httpRequest(httpClient));
             assertIsInternalServerError(e);
+        }
+
+        @Test
+        public void testUseSslConfigsOnlyWhenNecessary() throws Exception {
+            // See KAFKA-14816; we want to make sure that even if the worker is configured with invalid SSL properties,
+            // REST requests only fail if we try to contact a URL using HTTPS (but not HTTP)
+            int statusCode = Response.Status.OK.getStatusCode();
+            TestDTO expectedResponse = new TestDTO("someContent");
+            setupHttpClient(statusCode, toJsonString(expectedResponse));
+
+            String requestSignatureAlgorithm = "HmacSHA1";
+            assertDoesNotThrow(() -> httpRequest(httpClient, requestSignatureAlgorithm, false));
+            assertThrows(RuntimeException.class, () -> httpRequest(httpClient, requestSignatureAlgorithm, true));
         }
     }
 