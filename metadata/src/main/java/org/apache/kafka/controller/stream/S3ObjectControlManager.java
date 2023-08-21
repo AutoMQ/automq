6e064d0574143511bf3e6a0cec66cc8d30681d69
TheR1sing3un <ther1sing3un@163.com>
2023-08-21 21:17:02 +0800

    feat(s3): separate objects metadata from streams metadata
    1. separate objects metadata from streams metadata
    2. using a
    smaller memory used data structure in broker's replayed metadata cache
    
    Signed-off-by: TheR1sing3un <ther1sing3un@163.com>

--- metadata/src/main/java/org/apache/kafka/controller/stream/S3ObjectControlManager.java
+++ metadata/src/main/java/org/apache/kafka/controller/stream/S3ObjectControlManager.java
@@ -19,8 +19,13 @@ package org.apache.kafka.controller.stream;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import org.apache.kafka.common.metadata.RemoveS3ObjectRecord;
+import org.apache.kafka.common.metadata.S3ObjectRecord;
 import org.apache.kafka.common.utils.LogContext;
+import org.apache.kafka.controller.stream.S3ObjectKeyGeneratorManager.GenerateContextV0;
+import org.apache.kafka.metadata.stream.S3Config;
 import org.apache.kafka.metadata.stream.S3Object;
+import org.apache.kafka.metadata.stream.S3ObjectState;
 import org.apache.kafka.timeline.SnapshotRegistry;
 import org.apache.kafka.timeline.TimelineHashMap;
 import org.slf4j.Logger;
@@ -34,20 +39,28 @@ public class S3ObjectControlManager {
     
     private final TimelineHashMap<Long/*objectId*/, S3Object> objectsMetadata;
 
+    private final String clusterId;
+
+    private final S3Config config;
+
     /**
      * The objectId of the next object to be applied. (start from 0)
      */
     private Long nextApplyObjectId = 0L;
     
-    // TODO: add timer task to periodically check if there are objects to be destroyed or created
+    // TODO: add timer task to periodically check if there are objects to be destroyed or expired
     private final Queue<Long/*objectId*/> appliedObjects;
     private final Queue<Long/*objectId*/> markDestroyedObjects;
     
     public S3ObjectControlManager(
         SnapshotRegistry snapshotRegistry,
-        LogContext logContext) {
+        LogContext logContext,
+        String clusterId,
+        S3Config config) {
         this.snapshotRegistry = snapshotRegistry;
         this.log = logContext.logger(S3ObjectControlManager.class);
+        this.clusterId = clusterId;
+        this.config = config;
         this.objectsMetadata = new TimelineHashMap<>(snapshotRegistry, 0);
         this.appliedObjects = new LinkedList<>();
         this.markDestroyedObjects = new LinkedList<>();
@@ -56,5 +69,18 @@ public class S3ObjectControlManager {
     public Long appliedObjectNum() {
         return nextApplyObjectId;
     }
+
+    public void replay(S3ObjectRecord record) {
+        GenerateContextV0 ctx = new GenerateContextV0(clusterId, record.objectId());
+        String objectKey = S3ObjectKeyGeneratorManager.getByVersion(0).generate(ctx);
+        S3Object object = new S3Object(record.objectId(), record.objectSize(), objectKey,
+            record.appliedTimeInMs(), record.expiredTimeInMs(), record.committedTimeInMs(), record.destroyedTimeInMs(), S3ObjectState.fromByte(record.objectState()));
+        objectsMetadata.put(record.objectId(), object);
+    }
+
+    public void replay(RemoveS3ObjectRecord record) {
+        objectsMetadata.remove(record.objectId());
+    }
+
     
 }
