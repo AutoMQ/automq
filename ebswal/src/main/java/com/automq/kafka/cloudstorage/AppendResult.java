d26bdc8bbc901e01a55f33310ab03387cacbe4a3
Bruce <hulk@automq.com>
2023-09-03 16:07:23 +0800

    feat(ebs-wal) add api

--- ebswal/src/main/java/com/automq/kafka/cloudstorage/AppendResult.java
+++ ebswal/src/main/java/com/automq/kafka/cloudstorage/AppendResult.java
@@ -1,18 +1,21 @@
 package com.automq.kafka.cloudstorage;
 
 public interface AppendResult {
+    // 预分配好的 Reocord 存储的起始位置
     long beginOffset();
 
     class CallbackResult extends Object {
-        long pendingIOFistCommitOffset;
+        // Pending IO Window 的最小 Offset，此 Offset 之前的数据已经全部成功写入存储设备
+        long confirmedMinOffset;
+        // 预分配好的 Reocord 存储的起始位置
         long beginOffset;
 
-        public long getPendingIOFistCommitOffset() {
-            return pendingIOFistCommitOffset;
+        public long getConfirmedMinOffset() {
+            return confirmedMinOffset;
         }
 
-        public void setPendingIOFistCommitOffset(long pendingIOFistCommitOffset) {
-            this.pendingIOFistCommitOffset = pendingIOFistCommitOffset;
+        public void setConfirmedMinOffset(long confirmedMinOffset) {
+            this.confirmedMinOffset = confirmedMinOffset;
         }
 
         public long getBeginOffset() {
--- ebswal/src/main/java/com/automq/kafka/cloudstorage/FastWAL.java
+++ ebswal/src/main/java/com/automq/kafka/cloudstorage/FastWAL.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  * 是一个从零开始无限增长的 WAL，实际实现会使用块设备，每次写入采用块对齐方式。Record 之间非连续存储。
+ * 如果上一次是优雅关闭，重新启动后，read 返回的数据为空集合。
  */
 public interface FastWAL {
 
@@ -39,9 +40,19 @@ public interface FastWAL {
      */
     void start();
 
+    /**
+     * 阻止新的 Append 写入，等待已经写入的 Append 完成。
+     * 为优雅关闭 EBS WAL Service 服务。
+     * 第一步：调用 stopAppending() 方法，阻止新的 Append 写入。
+     * 第二步：将所有数据上传 S3
+     * 第三步：调用 trim() 方法，删除本地数据
+     * 第四步：调用 shutdown() 方法，关闭线程
+     */
+    void stopAppending();
+
 
     /**
-     * 关闭线程，保存元数据
+     * 关闭线程，保存元数据，其中包含 trim offset。
      */
     void shutdown();
 
@@ -54,8 +65,5 @@ public interface FastWAL {
     List<RecordEntity> read();
 
     void trim(long offset);
-
-
-    long trimedOffset();
 }
 
