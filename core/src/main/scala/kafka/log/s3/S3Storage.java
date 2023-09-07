71636d9be455cda0fab4589090b6fc7f2095954e
Robin Han <hanxvdovehx@gmail.com>
2023-09-07 15:33:23 +0800

    fix: fix checkstyle
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- core/src/main/scala/kafka/log/s3/S3Storage.java
+++ core/src/main/scala/kafka/log/s3/S3Storage.java
@@ -30,7 +30,6 @@ import org.apache.kafka.common.utils.ThreadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.concurrent.NotThreadSafe;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
