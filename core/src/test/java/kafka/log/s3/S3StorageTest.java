1f0ca14fee193c4c1af7ce63418b07e5333f333b
Robin Han <hanxvdovehx@gmail.com>
2023-09-08 13:59:21 +0800

    fix: fix unit test
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- core/src/test/java/kafka/log/s3/S3StorageTest.java
+++ core/src/test/java/kafka/log/s3/S3StorageTest.java
@@ -39,6 +39,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -132,7 +133,7 @@ public class S3StorageTest {
 
     @Test
     public void testUploadWALObject_sequence() throws ExecutionException, InterruptedException, TimeoutException {
-        List<CompletableFuture<Long>> objectIdCfList = new LinkedList<>();
+        List<CompletableFuture<Long>> objectIdCfList = new CopyOnWriteArrayList<>();
         doAnswer(invocation -> {
             CompletableFuture<Long> objectIdCf = new CompletableFuture<>();
             objectIdCfList.add(objectIdCf);
@@ -152,7 +153,7 @@ public class S3StorageTest {
         // sequence get objectId
         verify(objectManager, timeout(1000).times(1)).prepareObject(anyInt(), anyLong());
 
-        List<CompletableFuture<CommitWALObjectResponse>> commitCfList = new LinkedList<>();
+        List<CompletableFuture<CommitWALObjectResponse>> commitCfList = new CopyOnWriteArrayList<>();
         doAnswer(invocation -> {
             CompletableFuture<CommitWALObjectResponse> cf = new CompletableFuture<>();
             commitCfList.add(cf);
