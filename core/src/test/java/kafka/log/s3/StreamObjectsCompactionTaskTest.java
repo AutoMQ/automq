146e36424c6cce115f16c87a5746a42ec828afec
Robin Han <hanxvdovehx@gmail.com>
2023-09-14 17:32:29 +0800

    fix: checkstyle
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- core/src/test/java/kafka/log/s3/StreamObjectsCompactionTaskTest.java
+++ core/src/test/java/kafka/log/s3/StreamObjectsCompactionTaskTest.java
@@ -25,7 +25,6 @@ import java.util.Queue;
 import java.util.Stack;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
