b0696c7f7d1a6062fc52ef6f69b6fbc3b77a72de
SSpirits <admin@lv5.moe>
2023-12-26 17:04:42 +0800

    feat(stream): implement MemoryMetadataManager (#854)
    Signed-off-by: SSpirits <admin@lv5.moe>

---
+++ s3stream/src/test/java/com/automq/stream/s3/objects/ObjectManagerTest.java
@@ -0,0 +1,228 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.automq.stream.s3.objects;
+
+import com.automq.stream.s3.memory.MemoryMetadataManager;
+import com.automq.stream.s3.metadata.ObjectUtils;
+import com.automq.stream.s3.metadata.S3ObjectMetadata;
+import com.automq.stream.s3.metadata.StreamMetadata;
+import com.automq.stream.s3.metadata.StreamOffsetRange;
+import com.automq.stream.s3.streams.StreamManager;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ObjectManagerTest {
+    private StreamManager streamManager;
+    private ObjectManager objectManager;
+
+    @BeforeEach
+    void setUp() {
+        MemoryMetadataManager metadataManager = new MemoryMetadataManager();
+        streamManager = metadataManager;
+        objectManager = metadataManager;
+    }
+
+    @Test
+    void prepareObject() {
+        assertEquals(0, objectManager.prepareObject(1, 1000).join());
+        assertEquals(1, objectManager.prepareObject(1, 1000).join());
+        assertEquals(2, objectManager.prepareObject(8, 1000).join());
+        assertEquals(10, objectManager.prepareObject(1, 1000).join());
+    }
+
+    @Test
+    void testCommitAndCompact() {
+        Long streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+        streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+        streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+
+        CommitStreamSetObjectRequest request = new CommitStreamSetObjectRequest();
+        // Commit stream set object with stream 0 offset [0, 3) and stream 1 offset [0, 5).
+        ArrayList<ObjectStreamRange> streamRangeList = new ArrayList<>();
+        streamRangeList.add(new ObjectStreamRange(0, 0, 0, 3, 300));
+        streamRangeList.add(new ObjectStreamRange(1, 0, 0, 5, 500));
+        request.setStreamRanges(streamRangeList);
+        request.setOrderId(0);
+
+        // Commit stream object with stream 2 offset [0, 10).
+        List<StreamObject> streamObjectList = new ArrayList<>();
+        StreamObject streamObject = new StreamObject();
+        streamObject.setObjectId(1);
+        streamObject.setStreamId(2);
+        streamObject.setStartOffset(0);
+        streamObject.setEndOffset(10);
+        streamObjectList.add(streamObject);
+        request.setStreamObjects(streamObjectList);
+        objectManager.commitStreamSetObject(request).join();
+
+        List<StreamMetadata> streamMetadataList = streamManager.getStreams(List.of(0L, 1L, 2L)).join();
+        assertEquals(3, streamMetadataList.size());
+        assertEquals(3, streamMetadataList.get(0).getEndOffset());
+        assertEquals(5, streamMetadataList.get(1).getEndOffset());
+        assertEquals(10, streamMetadataList.get(2).getEndOffset());
+
+        List<S3ObjectMetadata> streamSetObjectMetadataList = objectManager.getServerObjects().join();
+        assertEquals(1, streamSetObjectMetadataList.size());
+        S3ObjectMetadata streamSetMetadata = streamSetObjectMetadataList.get(0);
+        List<StreamOffsetRange> ranges = streamSetMetadata.getOffsetRanges();
+        assertEquals(2, ranges.size());
+
+        assertEquals(0, ranges.get(0).getStreamId());
+        assertEquals(0, ranges.get(0).getStartOffset());
+        assertEquals(3, ranges.get(0).getEndOffset());
+
+        assertEquals(1, ranges.get(1).getStreamId());
+        assertEquals(0, ranges.get(1).getStartOffset());
+        assertEquals(5, ranges.get(1).getEndOffset());
+
+        List<S3ObjectMetadata> streamObjectMetadataList = objectManager.getStreamObjects(2, 0, 10, 100).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        ranges = streamObjectMetadataList.get(0).getOffsetRanges();
+        assertEquals(1, ranges.size());
+        assertEquals(2, ranges.get(0).getStreamId());
+        assertEquals(0, ranges.get(0).getStartOffset());
+        assertEquals(10, ranges.get(0).getEndOffset());
+
+        // Compact stream set object and commit stream object.
+        request = new CommitStreamSetObjectRequest();
+        request.setObjectId(ObjectUtils.NOOP_OBJECT_ID);
+        request.setCompactedObjectIds(List.of(0L));
+
+        streamObjectList = new ArrayList<>();
+        streamObject = new StreamObject();
+        streamObject.setObjectId(2);
+        streamObject.setStreamId(0);
+        streamObject.setStartOffset(0);
+        streamObject.setEndOffset(3);
+        streamObjectList.add(streamObject);
+
+        streamObject = new StreamObject();
+        streamObject.setObjectId(3);
+        streamObject.setStreamId(1);
+        streamObject.setStartOffset(0);
+        streamObject.setEndOffset(5);
+        streamObjectList.add(streamObject);
+
+        streamObject = new StreamObject();
+        streamObject.setObjectId(4);
+        streamObject.setStreamId(2);
+        streamObject.setStartOffset(10);
+        streamObject.setEndOffset(20);
+        streamObjectList.add(streamObject);
+
+        request.setStreamObjects(streamObjectList);
+        objectManager.commitStreamSetObject(request).join();
+
+        streamSetObjectMetadataList = objectManager.getServerObjects().join();
+        assertEquals(0, streamSetObjectMetadataList.size());
+
+        streamObjectMetadataList = objectManager.getStreamObjects(0, 0, 10, 100).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        streamObjectMetadataList = objectManager.getStreamObjects(1, 0, 10, 100).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        streamObjectMetadataList = objectManager.getStreamObjects(2, 0, 10, 100).join();
+        assertEquals(2, streamObjectMetadataList.size());
+
+        // Compact stream object.
+        objectManager.compactStreamObject(new CompactStreamObjectRequest(5, 2000, 2, 0, 20, List.of(1L, 4L))).join();
+        streamObjectMetadataList = objectManager.getStreamObjects(2, 0, 10, 100).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        ranges = streamObjectMetadataList.get(0).getOffsetRanges();
+        assertEquals(1, ranges.size());
+        assertEquals(2, ranges.get(0).getStreamId());
+        assertEquals(0, ranges.get(0).getStartOffset());
+        assertEquals(20, ranges.get(0).getEndOffset());
+    }
+
+    @Test
+    void testGetObject() {
+        // Create and open stream 0 and 1.
+        Long streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+        streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+
+        CommitStreamSetObjectRequest request = new CommitStreamSetObjectRequest();
+        // Commit stream set object with stream 0 offset [0, 3) and stream 1 offset [0, 5).
+        ArrayList<ObjectStreamRange> streamRangeList = new ArrayList<>();
+        streamRangeList.add(new ObjectStreamRange(0, 0, 0, 3, 300));
+        streamRangeList.add(new ObjectStreamRange(1, 0, 0, 5, 500));
+        request.setStreamRanges(streamRangeList);
+        request.setOrderId(0);
+
+        List<StreamObject> streamObjectList = new ArrayList<>();
+        StreamObject streamObject = new StreamObject();
+        streamObject.setObjectId(1);
+        streamObject.setStreamId(0);
+        streamObject.setStartOffset(3);
+        streamObject.setEndOffset(5);
+        streamObjectList.add(streamObject);
+
+        streamObject = new StreamObject();
+        streamObject.setObjectId(2);
+        streamObject.setStreamId(0);
+        streamObject.setStartOffset(5);
+        streamObject.setEndOffset(10);
+        streamObjectList.add(streamObject);
+
+        request.setStreamObjects(streamObjectList);
+        objectManager.commitStreamSetObject(request).join();
+
+        // Get object with stream 0 offset [0, 10).
+        List<S3ObjectMetadata> streamObjectMetadataList = objectManager.getObjects(0, 0, 10, 100).join();
+        assertEquals(3, streamObjectMetadataList.size());
+        // Get object with stream 0 offset [1, 9).
+        streamObjectMetadataList = objectManager.getObjects(0, 1, 9, 100).join();
+        assertEquals(3, streamObjectMetadataList.size());
+        // Get object with stream 0 offset [3, 4).
+        streamObjectMetadataList = objectManager.getObjects(0, 0, 1, 100).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        assertEquals(0, streamObjectMetadataList.get(0).objectId());
+        // Get object with limit 1.
+        streamObjectMetadataList = objectManager.getObjects(0, 0, 10, 1).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        assertEquals(0, streamObjectMetadataList.get(0).objectId());
+
+        // Get stream object with stream 0 offset [3, 10).
+        streamObjectMetadataList = objectManager.getStreamObjects(0, 3, 10, 100).join();
+        assertEquals(2, streamObjectMetadataList.size());
+        // Get stream object with stream 0 offset [5, 10).
+        streamObjectMetadataList = objectManager.getStreamObjects(0, 5, 10, 100).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        assertEquals(2, streamObjectMetadataList.get(0).objectId());
+        // Get object with limit 1.
+        streamObjectMetadataList = objectManager.getStreamObjects(0, 0, 10, 1).join();
+        assertEquals(1, streamObjectMetadataList.size());
+        assertEquals(1, streamObjectMetadataList.get(0).objectId());
+
+        // Get all stream set objects belonging current node.
+        streamObjectMetadataList = objectManager.getServerObjects().join();
+        assertEquals(1, streamObjectMetadataList.size());
+        // Change node id.
+        MemoryMetadataManager.advanceNodeId();
+        streamObjectMetadataList = objectManager.getServerObjects().join();
+        assertEquals(0, streamObjectMetadataList.size());
+    }
+}
---
+++ s3stream/src/test/java/com/automq/stream/s3/streams/StreamManagerTest.java
@@ -0,0 +1,147 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.automq.stream.s3.streams;
+
+import com.automq.stream.s3.memory.MemoryMetadataManager;
+import com.automq.stream.s3.metadata.StreamMetadata;
+import com.automq.stream.s3.metadata.StreamState;
+import com.automq.stream.s3.objects.CommitStreamSetObjectRequest;
+import com.automq.stream.s3.objects.ObjectManager;
+import com.automq.stream.s3.objects.StreamObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StreamManagerTest {
+    StreamManager streamManager;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        streamManager = new MemoryMetadataManager();
+    }
+
+    @Test
+    public void testCreateAndOpenStream() {
+        // Create and open stream with epoch 0.
+        Long streamId = streamManager.createStream().join();
+        StreamMetadata streamMetadata = streamManager.openStream(streamId, 0).join();
+        assertEquals(streamId, streamMetadata.getStreamId());
+        assertEquals(0, streamMetadata.getEpoch());
+        assertEquals(0, streamMetadata.getStartOffset());
+        assertEquals(0, streamMetadata.getEndOffset());
+        assertEquals(StreamState.OPENED, streamMetadata.getState());
+    }
+
+    @Test
+    public void testOpenAndCloseStream() {
+        // Create and open stream with epoch 0.
+        Long streamId = streamManager.createStream().join();
+        StreamMetadata streamMetadata = streamManager.openStream(streamId, 0).join();
+
+        // Close stream with epoch 1.
+        CompletableFuture<Void> future = streamManager.closeStream(streamId, 1);
+        assertEquals(StreamState.OPENED, streamMetadata.getState());
+        assertTrue(future.isCompletedExceptionally());
+
+        // Close stream with epoch 0.
+        streamManager.closeStream(streamId, 0).join();
+        assertEquals(StreamState.CLOSED, streamMetadata.getState());
+
+        // Open stream with epoch 0.
+        CompletableFuture<StreamMetadata> future1 = streamManager.openStream(streamId, 0);
+        assertTrue(future1.isCompletedExceptionally());
+
+        // Open stream with epoch 1.
+        streamMetadata = streamManager.openStream(streamId, 1).join();
+        assertEquals(streamId, streamMetadata.getStreamId());
+        assertEquals(1, streamMetadata.getEpoch());
+        assertEquals(0, streamMetadata.getStartOffset());
+        assertEquals(0, streamMetadata.getEndOffset());
+        assertEquals(StreamState.OPENED, streamMetadata.getState());
+
+        // Close stream with epoch 1.
+        streamManager.closeStream(streamId, 1).join();
+        assertEquals(StreamState.CLOSED, streamMetadata.getState());
+        streamManager.deleteStream(streamId, 1).join();
+        List<StreamMetadata> streamMetadataList = streamManager.getOpeningStreams().join();
+        assertEquals(0, streamMetadataList.size());
+    }
+
+    @Test
+    public void testTrimStream() {
+        // Create and open stream with epoch 0.
+        Long streamId = streamManager.createStream().join();
+        StreamMetadata streamMetadata = streamManager.openStream(streamId, 0).join();
+
+        // Trim stream with epoch 1.
+        CompletableFuture<Void> future = streamManager.trimStream(streamId, 1, 1);
+        assertTrue(future.isCompletedExceptionally());
+
+        // Trim stream to invalid offset.
+        CompletableFuture<Void> future1 = streamManager.trimStream(streamId, 0, -1);
+        assertTrue(future1.isCompletedExceptionally());
+        future1 = streamManager.trimStream(streamId, 0, 1);
+        assertTrue(future1.isCompletedExceptionally());
+
+        // Advance offset and trim stream.
+        CommitStreamSetObjectRequest request = new CommitStreamSetObjectRequest();
+        ArrayList<StreamObject> streamObjectList = new ArrayList<>();
+        StreamObject streamObject = new StreamObject();
+        streamObject.setStreamId(streamId);
+        streamObject.setStartOffset(0);
+        streamObject.setEndOffset(10);
+        streamObjectList.add(streamObject);
+        request.setStreamObjects(streamObjectList);
+        ((ObjectManager) streamManager).commitStreamSetObject(request).join();
+        assertEquals(10, streamMetadata.getEndOffset());
+
+        streamManager.trimStream(streamId, 0, 5).join();
+        assertEquals(5, streamMetadata.getStartOffset());
+    }
+
+    @Test
+    public void testGetStreams() {
+        ArrayList<Long> streamIds = new ArrayList<>();
+        // Create and open stream with epoch 0.
+        Long streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+        streamIds.add(streamId);
+
+        streamId = streamManager.createStream().join();
+        streamManager.openStream(streamId, 0).join();
+        streamIds.add(streamId);
+
+        // Get streams.
+        List<StreamMetadata> streamMetadataList = streamManager.getStreams(streamIds).join();
+        assertEquals(2, streamMetadataList.size());
+        assertEquals(streamId, streamMetadataList.get(1).getStreamId());
+        assertEquals(0, streamMetadataList.get(1).getEpoch());
+        assertEquals(0, streamMetadataList.get(1).getStartOffset());
+        assertEquals(0, streamMetadataList.get(1).getEndOffset());
+        assertEquals(StreamState.OPENED, streamMetadataList.get(1).getState());
+
+        streamIds.add(Long.MAX_VALUE);
+        streamMetadataList = streamManager.getStreams(streamIds).join();
+        assertEquals(2, streamMetadataList.size());
+    }
+}
