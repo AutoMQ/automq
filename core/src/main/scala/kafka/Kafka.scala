bebad6356b9fabd5a6994384a7ee8f54f1e9aed6
KaimingWan <kaiming.wan@automq.com>
2024-01-12 18:52:14 +0800

    fix(tool): fix code style
    Signed-off-by: KaimingWan <kaiming.wan@automq.com>

--- core/src/main/scala/kafka/Kafka.scala
+++ core/src/main/scala/kafka/Kafka.scala
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -71,93 +71,93 @@ object Kafka extends Logging {
         // This is a bit of an ugly crutch till we get a chance to rework the entire command line parsing
         optionParser.accepts("version", "Print version information and exit.")
 
-        if (args.isEmpty || args.contains("--help")) {
-            CommandLineUtils.printUsageAndDie(optionParser,
-                "USAGE: java [options] %s server.properties [--override property=value]*".format(this.getClass.getCanonicalName.split('$').head))
-        }
-
-        if (args.contains("--version")) {
-            CommandLineUtils.printVersionAndDie()
-        }
-
-        val props = Utils.loadProps(args(0))
-
-        if (args.length > 1) {
-            val options = optionParser.parse(args.slice(1, args.length): _*)
-
-            if (options.nonOptionArguments().size() > 0) {
-                CommandLineUtils.printUsageAndDie(optionParser, "Found non argument parameters: " + options.nonOptionArguments().toArray.mkString(","))
-            }
-
-            props ++= CommandLineUtils.parseKeyValueArgs(options.valuesOf(overrideOpt).asScala)
-        }
-        props
+    if (args.isEmpty || args.contains("--help")) {
+      CommandLineUtils.printUsageAndDie(optionParser,
+        "USAGE: java [options] %s server.properties [--override property=value]*".format(this.getClass.getCanonicalName.split('$').head))
     }
 
-    // For Zk mode, the API forwarding is currently enabled only under migration flag. We can
-    // directly do a static IBP check to see API forwarding is enabled here because IBP check is
-    // static in Zk mode.
-    private def enableApiForwarding(config: KafkaConfig) =
-        config.migrationEnabled && config.interBrokerProtocolVersion.isApiForwardingEnabled
-
-    private def buildServer(props: Properties): Server = {
-        val config = KafkaConfig.fromProps(props, false)
-        if (config.requiresZookeeper) {
-            new KafkaServer(
-                config,
-                Time.SYSTEM,
-                threadNamePrefix = None,
-                enableForwarding = enableApiForwarding(config)
-            )
-        } else {
-            new KafkaRaftServer(
-                config,
-                Time.SYSTEM,
-                threadNamePrefix = None
-            )
-        }
+    if (args.contains("--version")) {
+      CommandLineUtils.printVersionAndDie()
     }
 
-    def main(args: Array[String]): Unit = {
-        try {
-            val serverProps = getPropsFromArgs(args)
-            val server = buildServer(serverProps)
+    val props = Utils.loadProps(args(0))
 
-            try {
-                if (!OperatingSystem.IS_WINDOWS && !Java.isIbmJdk)
-                    new LoggingSignalHandler().register()
-            } catch {
-                case e: ReflectiveOperationException =>
-                    warn("Failed to register optional signal handler that logs a message when the process is terminated " +
-                        s"by a signal. Reason for registration failure is: $e", e)
-            }
+    if (args.length > 1) {
+      val options = optionParser.parse(args.slice(1, args.length): _*)
 
-            // attach shutdown handler to catch terminating signals as well as normal termination
-            Exit.addShutdownHook("kafka-shutdown-hook", {
-                try server.shutdown()
-                catch {
-                    case _: Throwable =>
-                        fatal("Halting Kafka.")
-                        // Calling exit() can lead to deadlock as exit() can be called multiple times. Force exit.
-                        Exit.halt(1)
-                }
-            })
+      if (options.nonOptionArguments().size() > 0) {
+        CommandLineUtils.printUsageAndDie(optionParser, "Found non argument parameters: " + options.nonOptionArguments().toArray.mkString(","))
+      }
 
-            try server.startup()
-            catch {
-                case e: Throwable =>
-                    // KafkaServer.startup() calls shutdown() in case of exceptions, so we invoke `exit` to set the status code
-                    fatal("Exiting Kafka due to fatal exception during startup.", e)
-                    Exit.exit(1)
-            }
+      props ++= CommandLineUtils.parseKeyValueArgs(options.valuesOf(overrideOpt).asScala)
+    }
+    props
+  }
 
-            server.awaitShutdown()
-        }
+  // For Zk mode, the API forwarding is currently enabled only under migration flag. We can
+  // directly do a static IBP check to see API forwarding is enabled here because IBP check is
+  // static in Zk mode.
+  private def enableApiForwarding(config: KafkaConfig) =
+    config.migrationEnabled && config.interBrokerProtocolVersion.isApiForwardingEnabled
+
+  private def buildServer(props: Properties): Server = {
+    val config = KafkaConfig.fromProps(props, false)
+    if (config.requiresZookeeper) {
+      new KafkaServer(
+        config,
+        Time.SYSTEM,
+        threadNamePrefix = None,
+        enableForwarding = enableApiForwarding(config)
+      )
+    } else {
+      new KafkaRaftServer(
+        config,
+        Time.SYSTEM,
+        threadNamePrefix = None
+      )
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    try {
+      val serverProps = getPropsFromArgs(args)
+      val server = buildServer(serverProps)
+
+      try {
+        if (!OperatingSystem.IS_WINDOWS && !Java.isIbmJdk)
+          new LoggingSignalHandler().register()
+      } catch {
+        case e: ReflectiveOperationException =>
+          warn("Failed to register optional signal handler that logs a message when the process is terminated " +
+            s"by a signal. Reason for registration failure is: $e", e)
+      }
+
+      // attach shutdown handler to catch terminating signals as well as normal termination
+      Exit.addShutdownHook("kafka-shutdown-hook", {
+        try server.shutdown()
         catch {
-            case e: Throwable =>
-                fatal("Exiting Kafka due to fatal exception", e)
-                Exit.exit(1)
+          case _: Throwable =>
+            fatal("Halting Kafka.")
+            // Calling exit() can lead to deadlock as exit() can be called multiple times. Force exit.
+            Exit.halt(1)
         }
-        Exit.exit(0)
+      })
+
+      try server.startup()
+      catch {
+        case e: Throwable =>
+          // KafkaServer.startup() calls shutdown() in case of exceptions, so we invoke `exit` to set the status code
+          fatal("Exiting Kafka due to fatal exception during startup.", e)
+          Exit.exit(1)
+      }
+
+      server.awaitShutdown()
     }
+    catch {
+      case e: Throwable =>
+        fatal("Exiting Kafka due to fatal exception", e)
+        Exit.exit(1)
+    }
+    Exit.exit(0)
+  }
 }
