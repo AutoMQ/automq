ac2f0c97b87cb4a852295654409652019d2b4bec
daniel-y <130526409+daniel-y@users.noreply.github.com>
2024-02-02 16:21:27 +0800

    chore: convert license to bsl to accelerate open source innovation (#913)
    Signed-off-by: daniel-y <daniel@automq.com>

--- s3stream/src/main/java/com/automq/stream/thirdparty/moe/cnkirito/kdio/DirectChannelImpl.java
+++ s3stream/src/main/java/com/automq/stream/thirdparty/moe/cnkirito/kdio/DirectChannelImpl.java
@@ -1,17 +1,12 @@
-/**
- * Copyright 2019 xujingfeng (kirito.moe@foxmail.com)
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
  */
 package com.automq.stream.thirdparty.moe.cnkirito.kdio;
 
