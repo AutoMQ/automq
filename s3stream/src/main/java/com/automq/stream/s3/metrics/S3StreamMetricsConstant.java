49444105c007a328e9b64195543c91c74ae20637
Shichao Nie <niesc@automq.com>
2024-02-05 12:12:17 +0800

    refactor(s3stream): remove _total suffix for counter metrics (#921)
    Signed-off-by: Shichao Nie <niesc@automq.com>

--- s3stream/src/main/java/com/automq/stream/s3/metrics/S3StreamMetricsConstant.java
+++ s3stream/src/main/java/com/automq/stream/s3/metrics/S3StreamMetricsConstant.java
@@ -63,16 +63,16 @@ public class S3StreamMetricsConstant {
         TimeUnit.MINUTES.toNanos(5)
     );
 
-    public static final String UPLOAD_SIZE_METRIC_NAME = "upload_size_total";
-    public static final String DOWNLOAD_SIZE_METRIC_NAME = "download_size_total";
-    public static final String OPERATION_COUNT_METRIC_NAME = "operation_count_total";
+    public static final String UPLOAD_SIZE_METRIC_NAME = "upload_size";
+    public static final String DOWNLOAD_SIZE_METRIC_NAME = "download_size";
+    public static final String OPERATION_COUNT_METRIC_NAME = "operation_count";
     public static final String OPERATION_LATENCY_METRIC_NAME = "operation_latency";
-    public static final String OBJECT_COUNT_METRIC_NAME = "object_count_total";
+    public static final String OBJECT_COUNT_METRIC_NAME = "object_count";
     public static final String OBJECT_STAGE_COST_METRIC_NAME = "object_stage_cost";
     public static final String OBJECT_UPLOAD_SIZE_METRIC_NAME = "object_upload_size";
     public static final String OBJECT_DOWNLOAD_SIZE_METRIC_NAME = "object_download_size";
-    public static final String NETWORK_INBOUND_USAGE_METRIC_NAME = "network_inbound_usage_total";
-    public static final String NETWORK_OUTBOUND_USAGE_METRIC_NAME = "network_outbound_usage_total";
+    public static final String NETWORK_INBOUND_USAGE_METRIC_NAME = "network_inbound_usage";
+    public static final String NETWORK_OUTBOUND_USAGE_METRIC_NAME = "network_outbound_usage";
     public static final String NETWORK_INBOUND_AVAILABLE_BANDWIDTH_METRIC_NAME = "network_inbound_available_bandwidth";
     public static final String NETWORK_OUTBOUND_AVAILABLE_BANDWIDTH_METRIC_NAME = "network_outbound_available_bandwidth";
     public static final String NETWORK_INBOUND_LIMITER_QUEUE_SIZE_METRIC_NAME = "network_inbound_limiter_queue_size";
@@ -90,8 +90,8 @@ public class S3StreamMetricsConstant {
     public static final String AVAILABLE_S3_INFLIGHT_READ_QUOTA_METRIC_NAME = "available_s3_inflight_read_quota";
     public static final String AVAILABLE_S3_INFLIGHT_WRITE_QUOTA_METRIC_NAME = "available_s3_inflight_write_quota";
     public static final String INFLIGHT_WAL_UPLOAD_TASKS_COUNT_METRIC_NAME = "inflight_wal_upload_tasks_count";
-    public static final String COMPACTION_READ_SIZE_METRIC_NAME = "compaction_read_size_total";
-    public static final String COMPACTION_WRITE_SIZE_METRIC_NAME = "compaction_write_size_total";
+    public static final String COMPACTION_READ_SIZE_METRIC_NAME = "compaction_read_size";
+    public static final String COMPACTION_WRITE_SIZE_METRIC_NAME = "compaction_write_size";
     public static final AttributeKey<String> LABEL_OPERATION_TYPE = AttributeKey.stringKey("operation_type");
     public static final AttributeKey<String> LABEL_OPERATION_NAME = AttributeKey.stringKey("operation_name");
     public static final AttributeKey<String> LABEL_SIZE_NAME = AttributeKey.stringKey("size");
