6deec0c42a21f6d2c09afd390c86bbc0c20f7833
Yu Ning <78631860+Chillax-0v0@users.noreply.github.com>
2024-02-07 15:59:16 +0800

    fix(auth): throw an exception when failed to create a credential from env (#793)
    Signed-off-by: Ning Yu <ningyu@automq.com>

--- kshell-sdk/src/main/java/com/automq/s3shell/sdk/auth/EnvVariableCredentialsProvider.java
+++ kshell-sdk/src/main/java/com/automq/s3shell/sdk/auth/EnvVariableCredentialsProvider.java
@@ -11,6 +11,7 @@
 package com.automq.s3shell.sdk.auth;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 
 public class EnvVariableCredentialsProvider {
@@ -18,9 +19,21 @@ public class EnvVariableCredentialsProvider {
     public static final String SECRET_KEY_NAME = "KAFKA_S3_SECRET_KEY";
 
     public static AwsCredentialsProvider get() {
+        return EnvVariableCredentialsProvider::create;
+    }
+
+    private static AwsCredentials create() {
         String accessKey = System.getenv(ACCESS_KEY_NAME);
         String secretKey = System.getenv(SECRET_KEY_NAME);
-        return () -> AwsBasicCredentials.create(accessKey, secretKey);
+        // According to the AWS documentation, when we fail to get the credentials in a credential provider,
+        // we should throw a RuntimeException to indicate that the provider is not able to provide the credential.
+        if (accessKey == null) {
+            throw new NullPointerException("Access key is not set");
+        }
+        if (secretKey == null) {
+            throw new NullPointerException("Secret key is not set");
+        }
+        return AwsBasicCredentials.create(accessKey, secretKey);
     }
 
     @Override
