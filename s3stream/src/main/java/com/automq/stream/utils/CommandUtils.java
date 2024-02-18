e30c295e7d085c515db6556ff9ce020fb2f4c845
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-02-18 18:05:59 +0800

    feat(issues801): clean expired stream objects when compact (#927)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- s3stream/src/main/java/com/automq/stream/utils/CommandUtils.java
+++ s3stream/src/main/java/com/automq/stream/utils/CommandUtils.java
@@ -13,15 +13,17 @@ package com.automq.stream.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 public class CommandUtils {
     public static CommandResult run(String... cmd) {
         try {
             Process p = Runtime.getRuntime().exec(cmd);
-            try (BufferedReader inputReader = p.inputReader();
-                 BufferedReader errorReader = p.errorReader()) {
-                String stdout = String.join("\n", inputReader.lines().toList());
-                String stderr = String.join("\n", errorReader.lines().toList());
+            try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                 BufferedReader errorReader = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+                String stdout = inputReader.lines().collect(Collectors.joining("\n"));
+                String stderr = errorReader.lines().collect(Collectors.joining("\n"));
                 int code = p.waitFor();
                 return new CommandResult(code, stdout, stderr);
             }
