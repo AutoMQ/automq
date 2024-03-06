6ccaf10609617fff5c913cd81a2ce83e14b62dff
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-03-06 11:46:12 +0800

    feat(streamaspect): add elastic time index (#881)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

---
+++ core/src/main/scala/kafka/log/streamaspect/ElasticResourceStatus.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+
+package kafka.log.streamaspect;
+
+public enum ElasticResourceStatus {
+    FENCED(false),
+    CLOSED(false),
+    OK(true);
+
+    private final boolean writable;
+
+    ElasticResourceStatus(boolean writable) {
+        this.writable = writable;
+    }
+
+    public boolean writable() {
+        return writable;
+    }
+}
---
+++ core/src/test/java/kafka/log/streamaspect/IStreamSliceSupplier.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+package kafka.log.streamaspect;
+
+import java.io.IOException;
+
+class IStreamSliceSupplier implements StreamSliceSupplier {
+    final ElasticStreamSlice slice;
+
+    public IStreamSliceSupplier(ElasticStreamSlice slice) {
+        this.slice = slice;
+    }
+
+    @Override
+    public ElasticStreamSlice get() throws IOException {
+        return slice;
+    }
+
+    @Override
+    public ElasticStreamSlice reset() throws IOException {
+        return new DefaultElasticStreamSlice(new MemoryClient.StreamImpl(2), SliceRange.of(0, Offsets.NOOP_OFFSET));
+    }
+}
