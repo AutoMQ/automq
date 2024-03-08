24c22ed6d3092287bb41ab7a087d597200567000
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-03-08 14:57:58 +0800

    feat(streamaspect): migrate piece of code (#903)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

---
+++ core/src/main/scala/kafka/log/streamaspect/RawKafkaMeta/RawKafkaMeta.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package kafka.log.streamaspect
+
+sealed trait RawKafkaMeta
+
+case object CleanerOffsetCheckpoint extends RawKafkaMeta
+
+case object LogStartOffsetCheckpoint extends RawKafkaMeta
+
+case object RecoveryPointCheckpoint extends RawKafkaMeta
+
+case object ReplicationOffsetCheckpoint extends RawKafkaMeta
---
+++ core/src/main/scala/kafka/log/streamaspect/client/Context.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package kafka.log.streamaspect.client;
+
+import kafka.server.BrokerServer;
+import kafka.server.KafkaConfig;
+
+public class Context {
+    // TODO: remove sync test mode, test should keep the same logic as production code.
+    private static boolean testMode = false;
+    public KafkaConfig config;
+    public BrokerServer brokerServer;
+
+    public static void enableTestMode() {
+        testMode = true;
+    }
+
+    public static boolean isTestMode() {
+        return testMode;
+    }
+}
