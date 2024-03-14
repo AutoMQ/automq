2036655c70699c7ef8b5de361fc33355a55d1da7
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-03-14 18:13:33 +0800

    feat(others): migrate other files (#930)
    * feat(others): migrate other files
    
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>
    
    * feat(test): add s3 unit test tag
    
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>
    
    ---------
    
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>
    Co-authored-by: KamiWan <kaiming.wan@automq.com>

--- bin/s3-test.sh
+++ bin/s3-test.sh
@@ -14,31 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ $# -lt 1 ];
-then
-	echo "USAGE: $0 [-daemon] server.properties [--override property=value]*"
-	exit 1
-fi
-base_dir=$(dirname $0)
-
-if [ "x$KAFKA_LOG4J_OPTS" = "x" ]; then
-    export KAFKA_LOG4J_OPTS="-Dlog4j.configuration=file:$base_dir/../config/log4j.properties"
-fi
-
-if [ "x$KAFKA_HEAP_OPTS" = "x" ]; then
-    export KAFKA_HEAP_OPTS="-Xmx6g -Xms6g -XX:MaxDirectMemorySize=6g -XX:MetaspaceSize=96m"
-fi
-
-EXTRA_ARGS=${EXTRA_ARGS-'-name kafkaServer -loggc'}
-
-COMMAND=$1
-case $COMMAND in
-  -daemon)
-    EXTRA_ARGS="-daemon "$EXTRA_ARGS
-    shift
-    ;;
-  *)
-    ;;
-esac
-
-exec $base_dir/kafka-run-class.sh $EXTRA_ARGS kafka.Kafka "$@"
+exec $(dirname $0)/kafka-run-class.sh kafka.admin.S3TestCommand "$@"
