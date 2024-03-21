3a6b151a4a830547b1b8a908ffc45351ce2b2d67
ipsum-0320 <trueman.0320@zju.edu.cn>
2024-03-21 14:14:38 +0800

    fix(core): remove unused pkg

--- core/src/main/java/kafka/kshell/log/scanner/Scanner.java
+++ core/src/main/java/kafka/kshell/log/scanner/Scanner.java
@@ -31,7 +31,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.HashMap;
