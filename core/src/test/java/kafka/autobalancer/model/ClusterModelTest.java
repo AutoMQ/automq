f9604266442c7810cabef5a3d30633014f94ab5e
Shichao Nie <niesc@automq.com>
2024-04-03 19:32:26 +0800

    fix(auto_balancer): avoid broker load affected by excluded topics (#1086)
    Signed-off-by: Shichao Nie <niesc@automq.com>

--- core/src/test/java/kafka/autobalancer/model/ClusterModelTest.java
+++ core/src/test/java/kafka/autobalancer/model/ClusterModelTest.java
@@ -17,6 +17,7 @@
 
 package kafka.autobalancer.model;
 
+import kafka.autobalancer.common.Resource;
 import kafka.autobalancer.common.types.RawMetricTypes;
 import kafka.autobalancer.metricsreporter.metric.TopicPartitionMetrics;
 import org.apache.kafka.common.TopicPartition;
@@ -31,8 +32,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Tag("S3Unit")
 public class ClusterModelTest {
@@ -226,8 +230,61 @@ public class ClusterModelTest {
     }
 
     @Test
-    public void testSnapshot() {
+    public void testExcludeTopics() {
+        RecordClusterModel clusterModel = new RecordClusterModel();
+        String topicName = "testTopic";
+        Uuid topicId = Uuid.randomUuid();
+        String topicName1 = "testTopic-1";
+        Uuid topicId1 = Uuid.randomUuid();
+        int partition = 0;
+        int brokerId = 1;
 
+        RegisterBrokerRecord registerBrokerRecord = new RegisterBrokerRecord()
+                .setBrokerId(brokerId);
+        clusterModel.onBrokerRegister(registerBrokerRecord);
+        TopicRecord topicRecord = new TopicRecord()
+                .setName(topicName)
+                .setTopicId(topicId);
+        clusterModel.onTopicCreate(topicRecord);
+        PartitionRecord partitionRecord = new PartitionRecord()
+                .setLeader(brokerId)
+                .setTopicId(topicId)
+                .setPartitionId(partition);
+        clusterModel.onPartitionCreate(partitionRecord);
+
+        TopicRecord topicRecord1 = new TopicRecord()
+                .setName(topicName1)
+                .setTopicId(topicId1);
+        clusterModel.onTopicCreate(topicRecord1);
+        PartitionRecord partitionRecord1 = new PartitionRecord()
+                .setLeader(brokerId)
+                .setTopicId(topicId1)
+                .setPartitionId(partition);
+        clusterModel.onPartitionCreate(partitionRecord1);
+        long now = System.currentTimeMillis();
+        clusterModel.updateBrokerMetrics(brokerId, new HashMap<>(), now);
+
+        TopicPartitionMetrics topicPartitionMetrics = new TopicPartitionMetrics(now, brokerId, "", topicName, partition);
+        topicPartitionMetrics.put(RawMetricTypes.TOPIC_PARTITION_BYTES_IN, 10);
+        topicPartitionMetrics.put(RawMetricTypes.TOPIC_PARTITION_BYTES_OUT, 10);
+        topicPartitionMetrics.put(RawMetricTypes.PARTITION_SIZE, 10);
+        clusterModel.updateTopicPartitionMetrics(topicPartitionMetrics.brokerId(),
+                new TopicPartition(topicName, partition), topicPartitionMetrics.getMetricValueMap(), topicPartitionMetrics.time());
+
+        TopicPartitionMetrics topicPartitionMetrics1 = new TopicPartitionMetrics(now, brokerId, "", topicName1, partition);
+        topicPartitionMetrics1.put(RawMetricTypes.TOPIC_PARTITION_BYTES_IN, 60);
+        topicPartitionMetrics1.put(RawMetricTypes.TOPIC_PARTITION_BYTES_OUT, 50);
+        topicPartitionMetrics1.put(RawMetricTypes.PARTITION_SIZE, 10);
+        clusterModel.updateTopicPartitionMetrics(topicPartitionMetrics1.brokerId(),
+                new TopicPartition(topicName1, partition), topicPartitionMetrics1.getMetricValueMap(), topicPartitionMetrics1.time());
+
+        ClusterModelSnapshot snapshot = clusterModel.snapshot(Collections.emptySet(), Set.of(topicName1), 10000);
+        Collection<TopicPartitionReplicaUpdater.TopicPartitionReplica> replicas = snapshot.replicasFor(brokerId);
+        Assertions.assertEquals(1, replicas.size());
+        Assertions.assertEquals(topicName, replicas.iterator().next().getTopicPartition().topic());
+        Assertions.assertEquals(partition, replicas.iterator().next().getTopicPartition().partition());
+        Assertions.assertEquals(70, snapshot.broker(brokerId).load(Resource.NW_IN));
+        Assertions.assertEquals(60, snapshot.broker(brokerId).load(Resource.NW_OUT));
     }
 
     @Test
