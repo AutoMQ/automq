076d1e0acd96e3049af2304dc85251b13312bda3
Robin Han <hanxvdovehx@gmail.com>
2024-04-28 15:03:33 +0800

    feat(s3stream): active evict expired data
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- s3stream/src/test/java/com/automq/stream/s3/cache/blockcache/DataBlockCacheTest.java
+++ s3stream/src/test/java/com/automq/stream/s3/cache/blockcache/DataBlockCacheTest.java
@@ -19,6 +19,7 @@ import com.automq.stream.s3.TestUtils;
 import com.automq.stream.s3.metadata.S3ObjectMetadata;
 import com.automq.stream.s3.metadata.S3ObjectType;
 import com.automq.stream.s3.model.StreamRecordBatch;
+import com.automq.stream.utils.MockTime;
 import com.automq.stream.utils.threads.EventLoop;
 import io.netty.buffer.ByteBuf;
 import java.util.ArrayList;
@@ -44,17 +45,18 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@Tag("S3Unit")
-public class DataBlockCacheTest {
+@Tag("S3Unit") public class DataBlockCacheTest {
     static final long STREAM_ID = 233;
 
     EventLoop[] eventLoops;
+    MockTime time = new MockTime();
     DataBlockCache cache;
 
     @BeforeEach
     void setup() {
         eventLoops = new EventLoop[] {new EventLoop("")};
-        cache = new DataBlockCache(1024, eventLoops);
+        time = new MockTime();
+        cache = new DataBlockCache(1024, eventLoops, time);
     }
 
     @Test
@@ -167,6 +169,40 @@ public class DataBlockCacheTest {
         }).get();
 
         AtomicReference<CompletableFuture<?>> cf3 = new AtomicReference<>();
+    }
+
+    @Test
+    public void testGetBlock_evictExpired() throws ExecutionException, InterruptedException, TimeoutException {
+        ObjectReader objectReader = mock(ObjectReader.class);
+        doAnswer(args -> {
+            DataBlockIndex idx = args.getArgument(0);
+            return CompletableFuture.completedFuture(new ObjectReader.DataBlockGroup(newDataBlockGroupBuf(idx)));
+        }).when(objectReader).read(any());
+        when(objectReader.metadata()).thenReturn(new S3ObjectMetadata(233L, 100000, S3ObjectType.STREAM));
+
+        AtomicReference<CompletableFuture<?>> cf1 = new AtomicReference<>();
+        eventLoops[0].submit(() -> {
+            cf1.set(cache.getBlock(objectReader, new DataBlockIndex(STREAM_ID, 0, 10, 1, 0, 100)).thenAccept(DataBlock::markUnread));
+        }).get();
+        cf1.get().get(1, TimeUnit.SECONDS);
+
+        AtomicReference<CompletableFuture<?>> cf2 = new AtomicReference<>();
+        time.setCurrentTimeMs(time.milliseconds() + DataBlockCache.DATA_TTL - 1);
+        eventLoops[0].submit(() -> {
+            cf2.set(cache.getBlock(objectReader, new DataBlockIndex(STREAM_ID, 100, 10, 1, 100, 200)).thenAccept(DataBlock::markUnread));
+        }).get();
+        cf2.get().get(1, TimeUnit.SECONDS);
+        assertEquals(2, cache.caches[0].blocks.size());
+
+        // expect idx1 is evicted cause of the expired
+        time.setCurrentTimeMs(time.milliseconds() + 2);
+        AtomicReference<CompletableFuture<?>> cf3 = new AtomicReference<>();
+        eventLoops[0].submit(() -> {
+            cf3.set(cache.getBlock(objectReader, new DataBlockIndex(STREAM_ID, 100, 10, 1, 100, 200)).thenAccept(DataBlock::markUnread));
+        }).get();
+        cf3.get().get(1, TimeUnit.SECONDS);
+        assertEquals(1, cache.caches[0].blocks.size());
+        assertEquals(100, cache.caches[0].blocks.keySet().iterator().next().dataBlockIndex.startOffset());
     }
 
     private ByteBuf newDataBlockGroupBuf(DataBlockIndex index) {
