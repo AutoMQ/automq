6a934fa0a0ef4bd99d10856824f2029703ccaebd
Ning Yu <ningyu@automq.com>
2024-05-30 21:47:59 +0800

    refactor: new `S3Operator` in `StreamClient`
    Signed-off-by: Ning Yu <ningyu@automq.com>

--- core/src/main/scala/kafka/server/streamaspect/client/s3/StreamClientFactory.java
+++ core/src/main/scala/kafka/server/streamaspect/client/s3/StreamClientFactory.java
@@ -11,12 +11,7 @@
 
 package kafka.server.streamaspect.client.s3;
 
-import com.automq.shell.auth.CredentialsProviderHolder;
-import com.automq.shell.auth.EnvVariableCredentialsProvider;
 import com.automq.stream.s3.Config;
-import com.automq.stream.s3.operator.DefaultS3Operator;
-import com.automq.stream.s3.operator.S3Operator;
-import java.util.List;
 import kafka.log.stream.s3.ConfigUtils;
 import kafka.server.streamaspect.client.Context;
 import org.apache.kafka.controller.stream.StreamClient;
@@ -24,13 +19,6 @@ import org.apache.kafka.controller.stream.StreamClient;
 public class StreamClientFactory {
     public static StreamClient get(Context context) {
         Config streamConfig = ConfigUtils.to(context.kafkaConfig);
-        S3Operator s3Operator = new DefaultS3Operator(
-            streamConfig.endpoint(),
-            streamConfig.region(),
-            streamConfig.bucket(),
-            streamConfig.forcePathStyle(),
-            List.of(CredentialsProviderHolder.getAwsCredentialsProvider(), EnvVariableCredentialsProvider.get()),
-            streamConfig.objectTagging());
-        return new StreamClient(streamConfig, s3Operator);
+        return new StreamClient(streamConfig);
     }
 }
--- metadata/src/main/java/org/apache/kafka/controller/stream/StreamClient.java
+++ metadata/src/main/java/org/apache/kafka/controller/stream/StreamClient.java
@@ -11,16 +11,26 @@
 
 package org.apache.kafka.controller.stream;
 
+import com.automq.shell.auth.CredentialsProviderHolder;
+import com.automq.shell.auth.EnvVariableCredentialsProvider;
 import com.automq.stream.s3.Config;
+import com.automq.stream.s3.operator.DefaultS3Operator;
 import com.automq.stream.s3.operator.S3Operator;
+import java.util.List;
 
 public class StreamClient {
     private final Config streamConfig;
     private final S3Operator s3Operator;
 
-    public StreamClient(Config streamConfig, S3Operator s3Operator) {
+    public StreamClient(Config streamConfig) {
         this.streamConfig = streamConfig;
-        this.s3Operator = s3Operator;
+        this.s3Operator = new DefaultS3Operator(
+            streamConfig.endpoint(),
+            streamConfig.region(),
+            streamConfig.bucket(),
+            streamConfig.forcePathStyle(),
+            List.of(CredentialsProviderHolder.getAwsCredentialsProvider(), EnvVariableCredentialsProvider.get()),
+            streamConfig.objectTagging());
     }
 
     public Config streamConfig() {
