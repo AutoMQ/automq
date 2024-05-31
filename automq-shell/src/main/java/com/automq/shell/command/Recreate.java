29d40707b3023f8c1057729fb036baf9aa18a731
SSpirits <admin@lv5.moe>
2024-05-31 11:06:06 +0800

    feat(shell): add recreate command (#1327)
    * feat(shell): add recreate command
    
    Signed-off-by: SSpirits <admin@lv5.moe>
    
    * feat(shell): add force parameter
    
    Signed-off-by: SSpirits <admin@lv5.moe>
    
    * feat(shell): fix help
    
    Signed-off-by: SSpirits <admin@lv5.moe>
    
    ---------
    
    Signed-off-by: SSpirits <admin@lv5.moe>

---
+++ automq-shell/src/main/java/com/automq/shell/command/Recreate.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package com.automq.shell.command;
+
+import com.automq.shell.AutoMQCLI;
+import com.automq.shell.stream.ClientKVClient;
+import com.automq.shell.util.CLIUtils;
+import com.automq.stream.api.KeyValue;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.NetworkClient;
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.errors.UnknownTopicOrPartitionException;
+import org.apache.kafka.common.metrics.Metrics;
+import org.apache.kafka.common.utils.LogContext;
+import org.apache.kafka.common.utils.Time;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "recreate", description = "Discard all data and recreate partition(s).", mixinStandardHelpOptions = true)
+public class Recreate implements Callable<Integer> {
+    @CommandLine.ParentCommand
+    AutoMQCLI cli;
+
+    @CommandLine.Option(names = {"-n", "--namespace"}, description = "The stream namespace.")
+    String namespace;
+
+    @CommandLine.Option(names = {"-t", "--topic-name"}, description = "The topic you want to recreate.", required = true)
+    String topicName;
+
+    @CommandLine.Option(names = {"-p", "--partition"}, description = "The partition you want to recreate.", required = true, paramLabel = "<partition>")
+    List<Integer> partitionList;
+
+    @CommandLine.Option(names = {"-f", "--force"}, description = "Confirm recreate the partition(s). @|bold,yellow This will result in the loss all data.|@")
+    boolean force;
+
+    @Override
+    public Integer call() throws Exception {
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, cli.bootstrapServer);
+        Admin admin = Admin.create(properties);
+
+        Optional<Node> nodeOptional = findAnyNode(admin);
+        if (nodeOptional.isEmpty()) {
+            System.err.println("No controller node found.");
+            return 1;
+        }
+
+        NetworkClient client = CLIUtils.buildNetworkClient("automq-cli", new AdminClientConfig(new Properties()), new Metrics(), Time.SYSTEM, new LogContext());
+        ClientKVClient clientKVClient = new ClientKVClient(client, nodeOptional.get());
+
+        if (StringUtils.isBlank(namespace)) {
+            String clusterId = admin.describeCluster().clusterId().get();
+            namespace = "_kafka_" + clusterId;
+        }
+
+        TopicDescription topicDescription;
+        try {
+            topicDescription = admin.describeTopics(List.of(topicName)).topicNameValues().get(topicName).get();
+        } catch (UnknownTopicOrPartitionException e) {
+            System.err.println("Topic " + topicName + " not found.");
+            return 1;
+        } catch (Exception e) {
+            System.err.println("Failed to describe topic " + topicName + ": " + e.getMessage());
+            return 1;
+        }
+
+        Uuid topicId = topicDescription.topicId();
+
+        String info;
+        if (!force) {
+            info = CommandLine.Help.Ansi.AUTO.string("@|bold Dry run mode, no partition will be recreated.|@");
+        } else {
+            info = CommandLine.Help.Ansi.AUTO.string("@|bold,yellow Remove the metadata stream related to specified partition, all data will be lost.|@");
+        }
+        System.out.println(info);
+
+        for (Integer partition : partitionList) {
+            String streamKey = formatStreamKey(namespace, topicId.toString(), partition);
+            KeyValue.Value value;
+            if (force) {
+                value = clientKVClient.deleteKV(streamKey);
+            } else {
+                value = clientKVClient.getKV(streamKey);
+            }
+
+            if (value == null || value.get() == null || value.get().remaining() < Long.BYTES) {
+                System.out.println("No stream found for partition " + topicName + "-" + partition);
+                continue;
+            }
+            System.out.println("The stream id related to partition " + topicName + "-" + partition + " is " + value.get().getLong());
+        }
+
+        return 0;
+    }
+
+    private String formatStreamKey(String namespace, String topicId, int partition) {
+        return namespace + "/" + topicId + "/" + partition;
+    }
+
+    private Optional<Node> findAnyNode(Admin admin) throws Exception {
+        return admin.describeCluster().nodes().get().stream().findFirst();
+    }
+}
---
+++ automq-shell/src/main/java/com/automq/shell/stream/ClientKVClient.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package com.automq.shell.stream;
+
+import com.automq.shell.metrics.S3MetricsExporter;
+import com.automq.stream.api.KeyValue;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import org.apache.kafka.clients.ClientRequest;
+import org.apache.kafka.clients.ClientResponse;
+import org.apache.kafka.clients.NetworkClient;
+import org.apache.kafka.clients.NetworkClientUtils;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.errors.NetworkException;
+import org.apache.kafka.common.message.DeleteKVsRequestData;
+import org.apache.kafka.common.message.DeleteKVsResponseData;
+import org.apache.kafka.common.message.GetKVsRequestData;
+import org.apache.kafka.common.message.GetKVsResponseData;
+import org.apache.kafka.common.message.PutKVsRequestData;
+import org.apache.kafka.common.message.PutKVsResponseData;
+import org.apache.kafka.common.protocol.Errors;
+import org.apache.kafka.common.requests.s3.DeleteKVsRequest;
+import org.apache.kafka.common.requests.s3.GetKVsRequest;
+import org.apache.kafka.common.requests.s3.PutKVsRequest;
+import org.apache.kafka.common.utils.Time;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ClientKVClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3MetricsExporter.class);
+
+    private final NetworkClient networkClient;
+    private final Node bootstrapServer;
+
+    public ClientKVClient(NetworkClient networkClient, Node bootstrapServer) throws IOException {
+        this.networkClient = networkClient;
+        this.bootstrapServer = bootstrapServer;
+        connect(bootstrapServer);
+    }
+
+    private void connect(Node node) throws IOException {
+        boolean ready = networkClient.isReady(node, Time.SYSTEM.milliseconds());
+        if (ready) {
+            return;
+        }
+
+        ready = NetworkClientUtils.awaitReady(networkClient, node, Time.SYSTEM, 1000);
+        if (!ready) {
+            throw new NetworkException("Failed to connect to the node " + node.id());
+        }
+    }
+
+    public KeyValue.Value getKV(String key) throws IOException {
+        long now = Time.SYSTEM.milliseconds();
+
+        LOGGER.trace("[ClientKVClient]: Get KV: {}", key);
+
+        GetKVsRequestData data = new GetKVsRequestData()
+            .setGetKeyRequests(List.of(new GetKVsRequestData.GetKVRequest().setKey(key)));
+
+        ClientRequest clientRequest = networkClient.newClientRequest(String.valueOf(bootstrapServer.id()),
+            new GetKVsRequest.Builder(data), now, true, 3000, null);
+
+        ClientResponse response = NetworkClientUtils.sendAndReceive(networkClient, clientRequest, Time.SYSTEM);
+        GetKVsResponseData responseData = (GetKVsResponseData) response.responseBody().data();
+
+        Errors code = Errors.forCode(responseData.errorCode());
+        if (Objects.requireNonNull(code) == Errors.NONE) {
+            return KeyValue.Value.of(responseData.getKVResponses().get(0).value());
+        }
+
+        throw code.exception();
+    }
+
+    public KeyValue.Value putKV(String key, byte[] value) throws IOException {
+        long now = Time.SYSTEM.milliseconds();
+
+        LOGGER.trace("[ClientKVClient]: put KV: {}", key);
+
+        PutKVsRequestData data = new PutKVsRequestData()
+            .setPutKVRequests(List.of(new PutKVsRequestData.PutKVRequest().setKey(key).setValue(value)));
+
+        ClientRequest clientRequest = networkClient.newClientRequest(String.valueOf(bootstrapServer.id()),
+            new PutKVsRequest.Builder(data), now, true, 3000, null);
+
+        ClientResponse response = NetworkClientUtils.sendAndReceive(networkClient, clientRequest, Time.SYSTEM);
+        PutKVsResponseData responseData = (PutKVsResponseData) response.responseBody().data();
+
+        Errors code = Errors.forCode(responseData.errorCode());
+        if (Objects.requireNonNull(code) == Errors.NONE) {
+            return KeyValue.Value.of(responseData.putKVResponses().get(0).value());
+        }
+
+        throw code.exception();
+    }
+
+    public KeyValue.Value deleteKV(String key) throws IOException {
+        long now = Time.SYSTEM.milliseconds();
+
+        LOGGER.trace("[ClientKVClient]: Delete KV: {}", key);
+        DeleteKVsRequestData data = new DeleteKVsRequestData()
+            .setDeleteKVRequests(List.of(new DeleteKVsRequestData.DeleteKVRequest().setKey(key)));
+
+        ClientRequest clientRequest = networkClient.newClientRequest(String.valueOf(bootstrapServer.id()),
+            new DeleteKVsRequest.Builder(data), now, true, 3000, null);
+
+        ClientResponse response = NetworkClientUtils.sendAndReceive(networkClient, clientRequest, Time.SYSTEM);
+        DeleteKVsResponseData responseData = (DeleteKVsResponseData) response.responseBody().data();
+
+        Errors code = Errors.forCode(responseData.errorCode());
+        if (Objects.requireNonNull(code) == Errors.NONE) {
+            return KeyValue.Value.of(responseData.deleteKVResponses().get(0).value());
+        }
+
+        throw code.exception();
+    }
+}
