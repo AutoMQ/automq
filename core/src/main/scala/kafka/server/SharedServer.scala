43e3c2845a1bd9b1d160d9c2178d78cc5996be92
Shichao Nie <niesc@automq.com>
2024-06-06 11:17:22 +0800

    fix(KAFKA-14644): Process should crash after failure in Raft IO thread (#13140) (#1361)
    fix(KAFKA-14644): Process should crash after failure in Raft IO thread
    
    Unexpected errors caught in the Raft IO thread should cause the process to stop. This is similar to the handling of exceptions in the controller.
    
    Reviewers: Colin P. McCabe <cmccabe@apache.org>
    
    Signed-off-by: Shichao Nie <niesc@automq.com>

--- core/src/main/scala/kafka/server/SharedServer.scala
+++ core/src/main/scala/kafka/server/SharedServer.scala
@@ -156,6 +156,12 @@ class SharedServer(
     }
   }
 
+  def raftManagerFaultHandler: FaultHandler = faultHandlerFactory.build(
+    name = "raft manager",
+    fatal = true,
+    action = () => {}
+  )
+
   /**
    * The fault handler to use when metadata loading fails.
    */
@@ -232,7 +238,9 @@ class SharedServer(
           time,
           metrics,
           threadNamePrefix,
-          controllerQuorumVotersFuture)
+          controllerQuorumVotersFuture,
+          raftManagerFaultHandler
+        )
         raftManager.startup()
 
         if (sharedServerConfig.processRoles.contains(ControllerRole)) {
