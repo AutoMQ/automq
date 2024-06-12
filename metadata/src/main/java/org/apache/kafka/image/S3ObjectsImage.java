bdbbcd9fcaab0d68519edb1d8902a89211503309
Robin Han <hanxvdovehx@gmail.com>
2024-06-12 16:28:13 +0800

    perf(metadata): use Timeline for objects image
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- metadata/src/main/java/org/apache/kafka/image/S3ObjectsImage.java
+++ metadata/src/main/java/org/apache/kafka/image/S3ObjectsImage.java
@@ -17,38 +17,45 @@
 
 package org.apache.kafka.image;
 
+import io.netty.util.AbstractReferenceCounted;
+import io.netty.util.ReferenceCounted;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Objects;
 import org.apache.kafka.common.metadata.AssignedS3ObjectIdRecord;
 import org.apache.kafka.image.writer.ImageWriter;
 import org.apache.kafka.image.writer.ImageWriterOptions;
 import org.apache.kafka.metadata.stream.S3Object;
 import org.apache.kafka.server.common.ApiMessageAndVersion;
+import org.apache.kafka.timeline.SnapshotRegistry;
+import org.apache.kafka.timeline.TimelineHashMap;
 
 /**
  * Represents the S3 objects in the metadata image.
  * <p>
  * This class is thread-safe.
  */
-public final class S3ObjectsImage {
+public final class S3ObjectsImage extends AbstractReferenceCounted {
 
-    public static final S3ObjectsImage EMPTY =
-        new S3ObjectsImage(-1, new DeltaMap<>(new int[] {1000, 10000}));
+    public static final S3ObjectsImage EMPTY = new S3ObjectsImage(-1, null, null, -1L);
 
     private long nextAssignedObjectId;
 
-    private final DeltaMap<Long/*objectId*/, S3Object> objectsMetadata;
+    private final TimelineHashMap<Long/*objectId*/, S3Object> objects;
+    private final SnapshotRegistry registry;
+    private final long epoch;
 
-    public S3ObjectsImage(long assignedObjectId, final DeltaMap<Long, S3Object> objectsMetadata) {
+    public S3ObjectsImage(long assignedObjectId, final TimelineHashMap<Long, S3Object> objects,
+        final SnapshotRegistry registry, final long epoch) {
         this.nextAssignedObjectId = assignedObjectId + 1;
-        this.objectsMetadata = objectsMetadata;
+        this.objects = objects;
+        this.registry = registry;
+        this.epoch = epoch;
     }
 
     public S3Object getObjectMetadata(long objectId) {
-        return this.objectsMetadata.get(objectId);
-    }
-
-    public DeltaMap<Long, S3Object> objectsMetadata() {
-        return objectsMetadata;
+        return this.objects.get(objectId, epoch);
     }
 
     public long nextAssignedObjectId() {
@@ -56,10 +63,13 @@ public final class S3ObjectsImage {
     }
 
     public void write(ImageWriter writer, ImageWriterOptions options) {
+        if (objects == null) {
+            return;
+        }
         writer.write(
             new ApiMessageAndVersion(
                 new AssignedS3ObjectIdRecord().setAssignedS3ObjectId(nextAssignedObjectId - 1), (short) 0));
-        objectsMetadata.forEach((k, v) -> writer.write(v.toRecord()));
+        objects.values(epoch).forEach(v -> writer.write(v.toRecord()));
     }
 
     @Override
@@ -71,16 +81,47 @@ public final class S3ObjectsImage {
             return false;
         }
         S3ObjectsImage that = (S3ObjectsImage) o;
-        return this.nextAssignedObjectId == that.nextAssignedObjectId &&
-            objectsMetadata.equals(that.objectsMetadata);
+        return this.nextAssignedObjectId == that.nextAssignedObjectId && objects().equals(that.objects());
     }
 
     public boolean isEmpty() {
-        return objectsMetadata.isEmpty();
+        return objects == null || objects.isEmpty(epoch);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nextAssignedObjectId, objectsMetadata);
+        return Objects.hash(nextAssignedObjectId, objects);
+    }
+
+    public Collection<Long> objectIds() {
+        return objects.keySet(epoch);
+    }
+
+    Collection<S3Object> objects() {
+        return objects == null ? Collections.emptyList() : new LinkedList<>(objects.values(epoch));
+    }
+
+    TimelineHashMap<Long, S3Object> timelineObjects() {
+        return objects;
+    }
+
+    SnapshotRegistry registry() {
+        return registry;
+    }
+
+    long epoch() {
+        return epoch;
+    }
+
+    @Override
+    protected void deallocate() {
+        if (registry != null) {
+            registry.deleteSnapshotsUpTo(epoch);
+        }
+    }
+
+    @Override
+    public ReferenceCounted touch(Object o) {
+        return this;
     }
 }
