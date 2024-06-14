765333cd2c1d625d39470cb066abef5e66b4e21f
jitokim <pigberger70@gmail.com>
2024-06-14 23:55:54 +0900

    replace server configuration to use static properties instead of hardcoded value in TransactionProducer class
    Signed-off-by: jitokim <pigberger70@gmail.com>

--- examples/src/main/java/kafka/examples/TransactionProducer.java
+++ examples/src/main/java/kafka/examples/TransactionProducer.java
@@ -24,7 +24,7 @@ public class TransactionProducer {
     public static void main(String... args) {
         // Set the required properties for running the Kafka producer.
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaProperties.BOOTSTRAP_SERVERS);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "my-transactional-id");
