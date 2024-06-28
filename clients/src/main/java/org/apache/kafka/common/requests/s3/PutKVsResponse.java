0ce3c44aed43dd57430e1069542487fbcf009f28
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-06-28 19:02:23 +0800

    fix(api): fix channel isn't switch when encounter NOT_CONTROLLER (#1489)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- clients/src/main/java/org/apache/kafka/common/requests/s3/PutKVsResponse.java
+++ clients/src/main/java/org/apache/kafka/common/requests/s3/PutKVsResponse.java
@@ -18,6 +18,7 @@
 package org.apache.kafka.common.requests.s3;
 
 import java.nio.ByteBuffer;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.kafka.common.message.PutKVsResponseData;
@@ -46,7 +47,10 @@ public class PutKVsResponse extends AbstractBatchResponse<PutKVResponse> {
 
     @Override
     public Map<Errors, Integer> errorCounts() {
-        return errorCounts(Errors.forCode(data.errorCode()));
+        Map<Errors, Integer> errorCounts = new HashMap<>();
+        updateErrorCounts(errorCounts, Errors.forCode(data.errorCode()));
+        data.putKVResponses().forEach(response -> updateErrorCounts(errorCounts, Errors.forCode(response.errorCode())));
+        return errorCounts;
     }
 
     @Override
