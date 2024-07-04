21db5bd43cefefa1cf8b0ecb28d57398b63b0dad
lifepuzzlefun <wjl_is_213@163.com>
2024-07-04 19:30:08 +0800

    perf(s3stream): use FastThreadLocal in WAL io executor (#1507)
    perf(s3stream): use FastThreadLocal in io executor

--- s3stream/src/main/java/com/automq/stream/utils/Threads.java
+++ s3stream/src/main/java/com/automq/stream/utils/Threads.java
@@ -40,6 +40,16 @@ public class Threads {
         };
     }
 
+    public static ExecutorService newFixedFastThreadLocalThreadPoolWithMonitor(int nThreads, String namePrefix, boolean isDaemen,
+                                                                               Logger logger) {
+        return S3StreamThreadPoolMonitor.createAndMonitor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, namePrefix, isDaemen, Integer.MAX_VALUE, throwable -> {
+            if (throwable != null) {
+                logger.error("[FATAL] Uncaught exception in executor thread {}", Thread.currentThread().getName(), throwable);
+            }
+            return null;
+        }, true);
+    }
+
     public static ExecutorService newFixedThreadPoolWithMonitor(int nThreads, String namePrefix, boolean isDaemen,
         Logger logger) {
         return S3StreamThreadPoolMonitor.createAndMonitor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, namePrefix, isDaemen, Integer.MAX_VALUE, throwable -> {
@@ -47,7 +57,7 @@ public class Threads {
                 logger.error("[FATAL] Uncaught exception in executor thread {}", Thread.currentThread().getName(), throwable);
             }
             return null;
-        });
+        }, false);
     }
 
     public static ScheduledExecutorService newSingleThreadScheduledExecutor(String name, boolean daemon,
