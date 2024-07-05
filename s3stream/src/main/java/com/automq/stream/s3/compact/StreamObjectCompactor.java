2a712f565429fe117f84ac94173c5bcbd1585bc6
lifepuzzlefun <wjl_is_213@163.com>
2024-07-05 17:33:37 +0800

    perf(s3stream): batch delete object when compact Composite object

--- s3stream/src/main/java/com/automq/stream/s3/compact/StreamObjectCompactor.java
+++ s3stream/src/main/java/com/automq/stream/s3/compact/StreamObjectCompactor.java
@@ -43,7 +43,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.BooleanSupplier;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
