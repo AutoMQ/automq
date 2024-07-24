c13911144c4827864126c82616ecb3c40ca948fb
Robin Han <hanxvdovehx@gmail.com>
2024-07-24 11:04:26 +0800

    fix(s3stream): fix version get npe by change the s3stream load timing
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- core/src/main/scala/kafka/log/streamaspect/ElasticLogManager.scala
+++ core/src/main/scala/kafka/log/streamaspect/ElasticLogManager.scala
@@ -13,186 +13,197 @@ package kafka.log.streamaspect
 
 import com.automq.stream.api.Client
 import com.automq.stream.s3.metadata.ObjectUtils
+import kafka.automq.AutoMQConfig
 import kafka.log.UnifiedLog
 import kafka.log.streamaspect.ElasticLogManager.NAMESPACE
 import kafka.log.streamaspect.cache.FileCache
 import kafka.log.streamaspect.client.{ClientFactoryProxy, Context}
 import kafka.server.{BrokerServer, BrokerTopicStats, KafkaConfig}
 import kafka.utils.Logging
-import org.apache.kafka.common.{TopicPartition, Uuid}
 import org.apache.kafka.common.utils.Time
+import org.apache.kafka.common.{TopicPartition, Uuid}
 import org.apache.kafka.server.util.Scheduler
 import org.apache.kafka.storage.internals.log.{LogConfig, LogDirFailureChannel, LogOffsetsListener, ProducerStateManagerConfig}
+import org.slf4j.LoggerFactory
 
 import java.io.File
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.{CompletableFuture, ConcurrentHashMap}
 import scala.jdk.CollectionConverters.ConcurrentMapHasAsScala
 
 class ElasticLogManager(val client: Client, val openStreamChecker: OpenStreamChecker) extends Logging {
-    this.logIdent = s"[ElasticLogManager] "
-    private val elasticLogs = new ConcurrentHashMap[TopicPartition, ElasticUnifiedLog]()
+  this.logIdent = s"[ElasticLogManager] "
+  private val elasticLogs = new ConcurrentHashMap[TopicPartition, ElasticUnifiedLog]()
 
-    def getOrCreateLog(dir: File,
-        config: LogConfig,
-        scheduler: Scheduler,
-        time: Time,
-        maxTransactionTimeoutMs: Int,
-        producerStateManagerConfig: ProducerStateManagerConfig,
-        brokerTopicStats: BrokerTopicStats,
-        producerIdExpirationCheckIntervalMs: Int,
-        logDirFailureChannel: LogDirFailureChannel,
-        topicId: Option[Uuid],
-        leaderEpoch: Long = 0,
-    ): ElasticUnifiedLog = {
-        val topicPartition = UnifiedLog.parseTopicPartitionName(dir)
-        val log = elasticLogs.get(topicPartition)
-        if (log != null) {
-            return log
-        }
-        var elasticLog: ElasticUnifiedLog = null
-        // Only Partition#makeLeader will create a new log, the ReplicaManager#asyncApplyDelta will ensure the same partition
-        // operate sequentially. So it's safe without lock
-        // ElasticLog new is a time cost operation.
-        elasticLog = ElasticUnifiedLog(
-            dir,
-            config,
-            scheduler,
-            time,
-            maxTransactionTimeoutMs,
-            producerStateManagerConfig,
-            brokerTopicStats,
-            producerIdExpirationCheckIntervalMs,
-            logDirFailureChannel,
-            topicId,
-            leaderEpoch,
-            logOffsetsListener = LogOffsetsListener.NO_OP_OFFSETS_LISTENER,
-            client,
-            NAMESPACE,
-            openStreamChecker
-        )
-        elasticLogs.put(topicPartition, elasticLog)
-        elasticLog
+  def getOrCreateLog(dir: File,
+    config: LogConfig,
+    scheduler: Scheduler,
+    time: Time,
+    maxTransactionTimeoutMs: Int,
+    producerStateManagerConfig: ProducerStateManagerConfig,
+    brokerTopicStats: BrokerTopicStats,
+    producerIdExpirationCheckIntervalMs: Int,
+    logDirFailureChannel: LogDirFailureChannel,
+    topicId: Option[Uuid],
+    leaderEpoch: Long = 0,
+  ): ElasticUnifiedLog = {
+    val topicPartition = UnifiedLog.parseTopicPartitionName(dir)
+    val log = elasticLogs.get(topicPartition)
+    if (log != null) {
+      return log
     }
+    var elasticLog: ElasticUnifiedLog = null
+    // Only Partition#makeLeader will create a new log, the ReplicaManager#asyncApplyDelta will ensure the same partition
+    // operate sequentially. So it's safe without lock
+    // ElasticLog new is a time cost operation.
+    elasticLog = ElasticUnifiedLog(
+      dir,
+      config,
+      scheduler,
+      time,
+      maxTransactionTimeoutMs,
+      producerStateManagerConfig,
+      brokerTopicStats,
+      producerIdExpirationCheckIntervalMs,
+      logDirFailureChannel,
+      topicId,
+      leaderEpoch,
+      logOffsetsListener = LogOffsetsListener.NO_OP_OFFSETS_LISTENER,
+      client,
+      NAMESPACE,
+      openStreamChecker
+    )
+    elasticLogs.put(topicPartition, elasticLog)
+    elasticLog
+  }
 
-    /**
-     * Delete elastic log by topic partition. Note that this method may not be called by the broker holding the partition.
-     *
-     * @param topicPartition topic partition
-     * @param epoch          epoch of the partition
-     */
-    def destroyLog(topicPartition: TopicPartition, topicId: Uuid, epoch: Long): Unit = {
-        // Log is removed from elasticLogs when partition closed.
-        try {
-            ElasticLog.destroy(client, NAMESPACE, topicPartition, topicId, epoch)
-        } catch {
-            case e: Throwable =>
-                // Even though the elastic log failed to be destroyed, the metadata has been deleted in controllers.
-                warn(s"Failed to destroy elastic log for $topicPartition. But we will ignore this exception. ", e)
-        }
+  /**
+   * Delete elastic log by topic partition. Note that this method may not be called by the broker holding the partition.
+   *
+   * @param topicPartition topic partition
+   * @param epoch          epoch of the partition
+   */
+  def destroyLog(topicPartition: TopicPartition, topicId: Uuid, epoch: Long): Unit = {
+    // Log is removed from elasticLogs when partition closed.
+    try {
+      ElasticLog.destroy(client, NAMESPACE, topicPartition, topicId, epoch)
+    } catch {
+      case e: Throwable =>
+        // Even though the elastic log failed to be destroyed, the metadata has been deleted in controllers.
+        warn(s"Failed to destroy elastic log for $topicPartition. But we will ignore this exception. ", e)
     }
+  }
 
-    /**
-     * Remove elastic log in the map.
-     *
-     * @param topicPartition topic partition
-     */
-    def removeLog(topicPartition: TopicPartition): Unit = {
-        elasticLogs.remove(topicPartition)
-    }
+  /**
+   * Remove elastic log in the map.
+   *
+   * @param topicPartition topic partition
+   */
+  def removeLog(topicPartition: TopicPartition): Unit = {
+    elasticLogs.remove(topicPartition)
+  }
 
-    def startup(): Unit = {
-        client.start()
-    }
+  def startup(): Unit = {
+    client.start()
+  }
 
-    def shutdownNow(): Unit = {
-        client.shutdown()
-    }
+  def shutdownNow(): Unit = {
+    client.shutdown()
+  }
 
 }
 
 object ElasticLogManager {
-    var INSTANCE: Option[ElasticLogManager] = None
-    var NAMESPACE = ""
-    private var isEnabled = false
+  val LOGGER = LoggerFactory.getLogger(ElasticLogManager.getClass)
+  var INSTANCE: Option[ElasticLogManager] = None
+  var NAMESPACE = ""
+  val INIT_FUTURE: CompletableFuture[Void] = new CompletableFuture[Void]()
+  private var isEnabled = false
 
-    def init(config: KafkaConfig, clusterId: String, broker: BrokerServer = null): Boolean = {
-        if (!config.elasticStreamEnabled) {
-            return false
-        }
-
-        val namespace = config.elasticStreamNamespace
-        NAMESPACE = if (namespace == null || namespace.isEmpty) {
-            "_kafka_" + clusterId
-        } else {
-            namespace
-        }
-        ObjectUtils.setNamespace(NAMESPACE)
-
-        val endpoint = config.elasticStreamEndpoint
-        if (endpoint == null) {
-            return false
-        }
-        val context = new Context()
-        context.config = config
-        context.brokerServer = broker
-        val openStreamChecker = if (broker != null) {
-            new DefaultOpenStreamChecker(broker.metadataCache)
-        } else {
-            OpenStreamChecker.NOOP
-        }
-        INSTANCE = Some(new ElasticLogManager(ClientFactoryProxy.get(context), openStreamChecker))
-        INSTANCE.foreach(_.startup())
-        ElasticLogSegment.txnCache = new FileCache(config.logDirs.head + "/" + "txnindex-cache", 100 * 1024 * 1024)
-        ElasticLogSegment.timeCache = new FileCache(config.logDirs.head + "/" + "timeindex-cache", 100 * 1024 * 1024)
-        true
+  def init(config: KafkaConfig, clusterId: String, broker: BrokerServer = null) = {
+    val namespace = config.elasticStreamNamespace
+    NAMESPACE = if (namespace == null || namespace.isEmpty) {
+      "_kafka_" + clusterId
+    } else {
+      namespace
     }
+    ObjectUtils.setNamespace(NAMESPACE)
 
-    def enable(shouldEnable: Boolean): Unit = {
-        isEnabled = shouldEnable
+    val endpoint = config.elasticStreamEndpoint
+    if (endpoint == null) {
+      throw new IllegalArgumentException("The " + AutoMQConfig.ELASTIC_STREAM_ENDPOINT_CONFIG + " must be set")
     }
-
-    def enabled(): Boolean = isEnabled
-
-    def removeLog(topicPartition: TopicPartition): Unit = {
-        INSTANCE.get.removeLog(topicPartition)
+    val context = new Context()
+    context.config = config
+    context.brokerServer = broker
+    val openStreamChecker = if (broker != null) {
+      new DefaultOpenStreamChecker(broker.metadataCache)
+    } else {
+      OpenStreamChecker.NOOP
     }
+    INSTANCE = Some(new ElasticLogManager(ClientFactoryProxy.get(context), openStreamChecker))
+    INSTANCE.foreach(_.startup())
+    ElasticLogSegment.txnCache = new FileCache(config.logDirs.head + "/" + "txnindex-cache", 100 * 1024 * 1024)
+    ElasticLogSegment.timeCache = new FileCache(config.logDirs.head + "/" + "timeindex-cache", 100 * 1024 * 1024)
+    INIT_FUTURE.complete(null)
+  }
 
-    def destroyLog(topicPartition: TopicPartition, topicId: Uuid, epoch: Long): Unit = {
-        INSTANCE.get.destroyLog(topicPartition, topicId, epoch)
+  def instance(): Option[ElasticLogManager] = {
+    INIT_FUTURE.get()
+    INSTANCE
+  }
+
+  def enable(shouldEnable: Boolean): Unit = {
+    isEnabled = shouldEnable
+  }
+
+  def enabled(): Boolean = isEnabled
+
+  def removeLog(topicPartition: TopicPartition): Unit = {
+    instance().get.removeLog(topicPartition)
+  }
+
+  def destroyLog(topicPartition: TopicPartition, topicId: Uuid, epoch: Long): Unit = {
+    instance().get.destroyLog(topicPartition, topicId, epoch)
+  }
+
+  def getElasticLog(topicPartition: TopicPartition): ElasticUnifiedLog = instance().get.elasticLogs.get(topicPartition)
+
+  def getAllElasticLogs: Iterable[ElasticUnifiedLog] = {
+    if (INSTANCE.isDefined) {
+      instance().get.elasticLogs.asScala.values
+    } else {
+      List.empty
     }
+  }
 
-    def getElasticLog(topicPartition: TopicPartition): ElasticUnifiedLog = INSTANCE.get.elasticLogs.get(topicPartition)
+  // visible for testing
+  def getOrCreateLog(dir: File,
+    config: LogConfig,
+    scheduler: Scheduler,
+    time: Time,
+    maxTransactionTimeoutMs: Int,
+    producerStateManagerConfig: ProducerStateManagerConfig,
+    brokerTopicStats: BrokerTopicStats,
+    producerIdExpirationCheckIntervalMs: Int,
+    logDirFailureChannel: LogDirFailureChannel,
+    topicId: Option[Uuid],
+    leaderEpoch: Long = 0): ElasticUnifiedLog = {
+    instance().get.getOrCreateLog(
+      dir,
+      config,
+      scheduler,
+      time,
+      maxTransactionTimeoutMs,
+      producerStateManagerConfig,
+      brokerTopicStats,
+      producerIdExpirationCheckIntervalMs,
+      logDirFailureChannel,
+      topicId,
+      leaderEpoch
+    )
+  }
 
-    def getAllElasticLogs: Iterable[ElasticUnifiedLog] = INSTANCE.get.elasticLogs.asScala.values
-
-    // visible for testing
-    def getOrCreateLog(dir: File,
-        config: LogConfig,
-        scheduler: Scheduler,
-        time: Time,
-        maxTransactionTimeoutMs: Int,
-        producerStateManagerConfig: ProducerStateManagerConfig,
-        brokerTopicStats: BrokerTopicStats,
-        producerIdExpirationCheckIntervalMs: Int,
-        logDirFailureChannel: LogDirFailureChannel,
-        topicId: Option[Uuid],
-        leaderEpoch: Long = 0): ElasticUnifiedLog = {
-        INSTANCE.get.getOrCreateLog(
-            dir,
-            config,
-            scheduler,
-            time,
-            maxTransactionTimeoutMs,
-            producerStateManagerConfig,
-            brokerTopicStats,
-            producerIdExpirationCheckIntervalMs,
-            logDirFailureChannel,
-            topicId,
-            leaderEpoch
-        )
-    }
-
-    def shutdown(): Unit = {
-        INSTANCE.foreach(_.shutdownNow())
-    }
+  def shutdown(): Unit = {
+    INSTANCE.foreach(_.shutdownNow())
+  }
 }
