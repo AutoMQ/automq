077eb983f7bf86d5746b9ec21264894f7a4aed23
cx <llzcxdb@163.com>
2024-07-26 14:49:03 +0800

    refactor(e2e): optimize autobalancer test cases and remove unused code

--- tests/kafkatest/automq/autobalancer_test.py
+++ tests/kafkatest/automq/autobalancer_test.py
@@ -1,11 +1,11 @@
-#  Copyright 2024, AutoMQ HK Limited.
+# Copyright 2024, AutoMQ HK Limited.
 #
-#  Use of this software is governed by the Business Source License
-#  included in the file BSL.md
+# Use of this software is governed by the Business Source License
+# included in the file BSL.md
 #
-#  As of the Change Date specified in that file, in accordance with
-#  the Business Source License, use of this software will be governed
-#  by the Apache License, Version 2.0
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
 #
 # Use of this software is governed by the Business Source License
 # included in the file BSL.md
@@ -16,8 +16,7 @@
 from ducktape.mark import parametrize
 from ducktape.mark.resource import cluster
 from ducktape.tests.test import Test
-from kafkatest.automq.automq_e2e_util import (run_simple_load, TOPIC, append_info,
-                                              JMX_BROKER_IN, JMX_BROKER_OUT, JMX_ONE_MIN)
+from kafkatest.automq.automq_e2e_util import (run_simple_load, TOPIC, append_info)
 from kafkatest.services.kafka import KafkaService
 
 # Configuration constants for the AutoBalancer
@@ -32,36 +31,6 @@ GOALS = 'autobalancer.controller.goals'
 EXCLUDE_TOPIC = 'autobalancer.controller.exclude.topics'
 EXCLUDE_BROKER = 'autobalancer.controller.exclude.broker.ids'
 METRIC_REPORTERS = 'metric.reporters'
-
-
-def validate_throughput(value_name, value_list, rate, exclude=None):
-    """
-    Validate if the throughput values are within the allowed deviation range.
-
-    :param value_name: Name of the value being validated
-    :param value_list: List of throughput values
-    :param rate: Allowed deviation rate
-    :param exclude: Set of broker indices to exclude from validation
-    :return: Tuple of success status and message
-    """
-    if exclude is None:
-        exclude = set()
-    filtered_values = [value for idx, value in enumerate(value_list) if idx not in exclude]
-    if not filtered_values:
-        return True, "No values to validate after applying exclusions."
-    success = True
-    msg = ''
-    avg = sum(filtered_values) / len(filtered_values)
-    for idx, value in enumerate(value_list):
-        if idx in exclude:
-            continue
-        deviation = abs(value - avg) / avg
-        if deviation > rate:
-            success = False
-            msg += (
-                f"{value_name} value {value}(broker_id:{idx}) deviates from the average {avg} by {deviation}, which "
-                f"exceeds the allowed range of {rate}. (throughput:{filtered_values})\n")
-    return success, msg
 
 
 def get_partition_count_per_broker(partition_data, exclude=None):
@@ -107,6 +76,15 @@ def check_partition_replicas(partition_data, exclude=None):
         success = False
         msg = "Brokers have different numbers of partition replicas: " + str(broker_replicas_count)
 
+    return success, msg
+
+
+def check_partition_eq(topic_info1, topic_info2):
+    success, msg = True, ''
+    success = str(topic_info1) == str(topic_info2)
+    topic_name = topic_info1.get('partitions')[0]['topic']
+    if not success:
+        msg += f"Topic {topic_name} was modified despite being excluded from AutoBalancer. Before: {topic_info1}, After: {topic_info2}"
     return success, msg
 
 
@@ -162,7 +140,7 @@ class AutoBalancerTest(Test):
 
         self.controller_num_nodes_override = 0
         if num_nodes == 3:
-            self.controller_num_nodes_override = 1 # only use one combined node
+            self.controller_num_nodes_override = 1  # only use one combined node
 
         self.kafka = KafkaService(self.context, num_nodes=num_nodes, zk=None,
                                   kafka_heap_opts="-Xmx2048m -Xms2048m",
@@ -177,16 +155,13 @@ class AutoBalancerTest(Test):
                                           }
                                       },
                                   },
-                                  jmx_object_names=['kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec',
-                                                    'kafka.server:type=BrokerTopicMetrics,name=BytesOutPerSec'],
-                                  jmx_attributes=['OneMinuteRate'],
                                   controller_num_nodes_override=self.controller_num_nodes_override
                                   )
         self.start = True
 
     @cluster(num_nodes=5)
     @parametrize(automq_num_nodes=2, partition=4, replica_assignment='1,1,1,2')
-    def test_throughput(self, automq_num_nodes, partition, replica_assignment):
+    def test_action(self, automq_num_nodes, partition, replica_assignment):
         """
         Test throughput distribution across brokers
         :param automq_num_nodes: Number of automq
@@ -205,26 +180,10 @@ class AutoBalancerTest(Test):
         success = success and success_
         msg = append_info(msg, success_, msg_)
 
-        broker = self.kafka
-        broker.read_jmx_output_all_nodes()
-        type_in = JMX_BROKER_IN + JMX_ONE_MIN
-        type_out = JMX_BROKER_OUT + JMX_ONE_MIN
-        broker_max_in_per_node = [node[type_in] for node in broker.maximum_jmx_value_per_node]
-        broker_max_out_per_node = [node[type_out] for node in broker.maximum_jmx_value_per_node]
-
-        deviation_percentage = self.avg_deviation + self.maximum_broker_deviation_percentage
-        success_, msg_ = validate_throughput('broker_max_in_per_node', broker_max_in_per_node, deviation_percentage)
-        success = success and success_
-        msg = append_info(msg, success_, msg_)
-
-        success_, msg_ = validate_throughput('broker_max_out_per_node', broker_max_out_per_node, deviation_percentage)
-        success = success and success_
-        msg = append_info(msg, success_, msg_)
-
         assert success, msg
 
-    @cluster(num_nodes=5)
-    @parametrize(automq_num_nodes=3, exclude_broker='1', partition=5, replica_assignment='1,2,2,2,3')
+    @cluster(num_nodes=4)
+    @parametrize(automq_num_nodes=2, exclude_broker='2', partition=4, replica_assignment='1,1,1,2')
     def test_broker_white_list(self, automq_num_nodes, exclude_broker, partition, replica_assignment):
         """
         Test broker exclusion functionality
@@ -242,19 +201,9 @@ class AutoBalancerTest(Test):
                         num_records=20000, throughput=1300)
         after = self.kafka.parse_describe_topic(self.kafka.describe_topic(TOPIC))
 
-        broker_list = [broker_id.strip() for broker_id in exclude_broker.split(',')]
-
-        success_, msg_ = check_partition_replicas(after, exclude=broker_list)
+        success_, msg_ = check_partition_eq(topic_info1=before, topic_info2=after)
         success = success and success_
         msg = append_info(msg, success_, msg_)
-
-        for broker_id in broker_list:
-            before_partition_count = get_partition_count_per_broker(before)[broker_id]
-            after_partition_count = get_partition_count_per_broker(after)[broker_id]
-            success_ = before_partition_count == after_partition_count
-            if not success_:
-                msg_ = f"Error at broker_id: {broker_id}. Expected partition count: {before_partition_count}, but got: {after_partition_count}.\n"
-                msg += msg_
 
         assert success, msg
 
@@ -296,8 +245,7 @@ class AutoBalancerTest(Test):
         topic1_after = self.kafka.parse_describe_topic(self.kafka.describe_topic(topic1))
         topic2_after = self.kafka.parse_describe_topic(self.kafka.describe_topic(topic2))
 
-        success_, msg_ = str(topic1_before) == str(
-            topic1_after), f"Topic {topic1} was modified despite being excluded from AutoBalancer. Before: {topic1_before}, After: {topic1_after}"
+        success_, msg_ = check_partition_eq(topic1_before, topic1_after)
         success = success and success_
         msg = append_info(msg, success_, msg_)
 
