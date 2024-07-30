286f01cd1edf631395880ed6f8d70d50db454d26
Shichao Nie <niesc@automq.com>
2024-07-30 16:57:06 +0800

    feat(metrics): upgrade OpenTelemetry dependencies versions (#1665)
    - OpenTelemetry SDK -> 1.40.0
    - OpenTelemetry-Instrument -> 2.6.0
    - Support exposing 'job' and 'instance' labels in Prometheus HTTP server exporter
    - Upgrade to stable JVM metrics semconv (https://github.com/open-telemetry/semantic-conventions/blob/main/docs/runtime/jvm-metrics.md)
    
    Signed-off-by: Shichao Nie <niesc@automq.com>

---
+++ core/src/main/scala/kafka/log/stream/s3/telemetry/MetricsConstants.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2024, AutoMQ HK Limited.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package kafka.log.stream.s3.telemetry;
+
+public class MetricsConstants {
+    public static final String SERVICE_NAME = "service.name";
+    public static final String SERVICE_INSTANCE = "service.instance.id";
+    public static final String HOST_NAME = "host.name";
+    public static final String INSTANCE = "instance";
+    public static final String JOB = "job";
+    public static final String NODE_TYPE = "node.type";
+}
