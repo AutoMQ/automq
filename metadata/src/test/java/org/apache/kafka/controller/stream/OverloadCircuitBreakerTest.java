4bf5d636cbaaf7ecd3854ad36c2d71fb0bbb1f69
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-07-31 19:03:25 +0800

    feat(issues1446): avoid accidentally fence brokers when controller overload (#1686)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

---
+++ metadata/src/test/java/org/apache/kafka/controller/stream/OverloadCircuitBreakerTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024, AutoMQ HK Limited.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package org.apache.kafka.controller.stream;
+
+import org.apache.kafka.common.utils.MockTime;
+import org.apache.kafka.common.utils.Time;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("S3Unit")
+public class OverloadCircuitBreakerTest {
+
+    @Test
+    public void test() {
+        Time time = new MockTime();
+        OverloadCircuitBreaker breaker = new OverloadCircuitBreaker(time);
+        assertFalse(breaker.isOverload());
+        breaker.overload();
+        assertTrue(breaker.isOverload());
+        breaker.success();
+        assertTrue(breaker.isOverload());
+        time.sleep(OverloadCircuitBreaker.HALF_OPEN_WINDOW_MS);
+        breaker.success();
+        assertFalse(breaker.isOverload());
+    }
+
+}
