236d9ad915717f05214c0ce588545418af811418
lifepuzzlefun <wjl_is_213@163.com>
2024-08-13 12:43:42 +0800

    fix(metadataimage): fix getObjects not complete cf when throw exception
    fix comment

--- metadata/src/main/java/org/apache/kafka/image/S3StreamsMetadataImage.java
+++ metadata/src/main/java/org/apache/kafka/image/S3StreamsMetadataImage.java
@@ -464,8 +464,7 @@ public final class S3StreamsMetadataImage extends AbstractReferenceCounted {
 
     private Optional<StreamOffsetRange> findStreamInStreamSetObject(GetObjectsContext ctx, S3StreamSetObject object) {
         if (object.ranges().length == 0) {
-            Optional<StreamOffsetRange> streamOffsetRangeOpt = ctx.object2range.get(object.objectId());
-            return streamOffsetRangeOpt != null ? streamOffsetRangeOpt : Optional.empty();
+            return ctx.object2range.get(object.objectId());
         } else {
             return object.find(ctx.streamId);
         }
