aa5fbd7f1fe4bb83adaaaa2df10fcbd37bff9a28
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2024-08-14 14:09:02 +0800

    fix(s3stream): fix AsyncLRUCache to support remove (#1786)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- s3stream/src/main/java/com/automq/stream/s3/cache/AsyncLRUCache.java
+++ s3stream/src/main/java/com/automq/stream/s3/cache/AsyncLRUCache.java
@@ -12,20 +12,22 @@
 package com.automq.stream.s3.cache;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AsyncLRUCache<K, V extends AsyncMeasurable> extends LRUCache<K, V> {
+public class AsyncLRUCache<K, V extends AsyncMeasurable> {
     private static final Logger LOGGER = LoggerFactory.getLogger(AsyncLRUCache.class);
-    private final int maxSize;
-    final AtomicInteger totalSize = new AtomicInteger(0);
+    private final long maxSize;
+    final AtomicLong totalSize = new AtomicLong(0);
+    final LRUCache<K, V> cache = new LRUCache<>();
     final Set<V> completedSet = new HashSet<>();
     final Set<V> removedSet = new HashSet<>();
 
-    public AsyncLRUCache(int maxSize) {
+    public AsyncLRUCache(long maxSize) {
         super();
         if (maxSize <= 0) {
             throw new IllegalArgumentException("maxSize must be positive");
@@ -33,13 +35,17 @@ public class AsyncLRUCache<K, V extends AsyncMeasurable> extends LRUCache<K, V> 
         this.maxSize = maxSize;
     }
 
-    @Override
     public synchronized void put(K key, V value) {
-        super.put(key, value);
+        V oldValue = cache.get(key);
+        if (oldValue != null && oldValue != value) {
+            cache.remove(key);
+            afterRemoveValue(oldValue);
+        }
+        cache.put(key, value);
         value.size().whenComplete((v, ex) -> {
             synchronized (AsyncLRUCache.this) {
                 if (ex != null) {
-                    remove(key);
+                    cache.remove(key);
                 } else if (!removedSet.contains(value)) {
                     completedSet.add(value);
                     if (totalSize.addAndGet(v) > maxSize) {
@@ -57,26 +63,65 @@ public class AsyncLRUCache<K, V extends AsyncMeasurable> extends LRUCache<K, V> 
         });
     }
 
-    private synchronized void evict() {
-        while (totalSize.get() > maxSize && cache.size() > 1) {
-            Optional.ofNullable(pop()).ifPresent(entry -> {
-                V value = entry.getValue();
-                try {
-                    boolean completed = completedSet.remove(value);
-                    if (completed) {
-                        totalSize.addAndGet(-value.size().get());
-                        value.close();
-                    } else {
-                        removedSet.add(value);
-                    }
-                } catch (Throwable e) {
-                    LOGGER.error("Failed to evict {}", value, e);
-                }
-            });
+    public synchronized V get(K key) {
+        return cache.get(key);
+    }
+
+
+    public synchronized boolean remove(K key) {
+        V value =  cache.get(key);
+        if (value == null) {
+            return false;
+        }
+        cache.remove(key);
+        afterRemoveValue(value);
+        return true;
+    }
+
+    private synchronized void afterRemoveValue(V value) {
+        try {
+            boolean completed = completedSet.remove(value);
+            if (completed) {
+                totalSize.addAndGet(-value.size().get());
+                value.close();
+            } else {
+                removedSet.add(value);
+            }
+        } catch (Throwable e) {
+            LOGGER.error("Failed to remove {}", value, e);
         }
     }
 
-    public int totalSize() {
+    public synchronized Map.Entry<K, V> pop() {
+        Map.Entry<K, V> entry = cache.pop();
+        if (entry != null) {
+            afterRemoveValue(entry.getValue());
+        }
+        return entry;
+    }
+
+    public synchronized int size() {
+        return cache.size();
+    }
+
+    public synchronized boolean containsKey(K key) {
+        return cache.containsKey(key);
+    }
+
+    public synchronized void clear() {
+        cache.clear();
+    }
+
+    public long totalSize() {
         return totalSize.get();
+    }
+
+    private synchronized void evict() {
+        while (totalSize.get() > maxSize && cache.size() > 1) {
+            Optional.ofNullable(cache.pop()).ifPresent(entry -> {
+                V value = entry.getValue();
+                afterRemoveValue(value);
+            });
+        }
     }
 }
