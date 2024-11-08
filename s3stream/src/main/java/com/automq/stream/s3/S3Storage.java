fb5bce82917276052cfe6f73fcef26595f8569b5
Yu Ning <78631860+Chillax-0v0@users.noreply.github.com>
2024-11-08 11:13:26 +0800

    fix(s3stream/storage): correct if condition on `awaitTermination` (#2137)
    Signed-off-by: Ning Yu <ningyu@automq.com>

--- s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
+++ s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
@@ -397,7 +397,7 @@ public class S3Storage implements Storage {
         deltaWAL.shutdownGracefully();
         backgroundExecutor.shutdown();
         try {
-            if (backgroundExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
+            if (!backgroundExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
                 LOGGER.warn("await backgroundExecutor timeout 10s");
             }
         } catch (InterruptedException e) {
