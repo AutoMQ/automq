bcdd7e174b904e802fe969a5e8f5c7780f0d4af1
Yu Ning <78631860+Chillax-0v0@users.noreply.github.com>
2024-11-15 10:31:30 +0800

    feat(backpressure): back pressure by system load (#2161)
    * feat(backpressure): init backpressure module
    
    Signed-off-by: Ning Yu <ningyu@automq.com>
    
    * feat(backpressure): implement `DefaultBackPressureManager`
    
    Signed-off-by: Ning Yu <ningyu@automq.com>
    
    * test(backpressure): test `DefaultBackPressureManager`
    
    Signed-off-by: Ning Yu <ningyu@automq.com>
    
    ---------
    
    Signed-off-by: Ning Yu <ningyu@automq.com>

---
+++ s3stream/src/main/java/com/automq/stream/s3/backpressure/Regulator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024, AutoMQ HK Limited.
+ *
+ * The use of this file is governed by the Business Source License,
+ * as detailed in the file "/LICENSE.S3Stream" included in this repository.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+package com.automq.stream.s3.backpressure;
+
+/**
+ * The Regulator class is responsible for controlling and limiting the rate of external requests.
+ * It provides methods to increase, decrease, and minimize the flow of incoming requests.
+ */
+public interface Regulator {
+
+    /**
+     * Increase the rate of incoming requests.
+     * If the rate is already at the maximum, this method does nothing.
+     */
+    void increase();
+
+    /**
+     * Decrease the rate of incoming requests.
+     * If the rate is already at the minimum, this method does nothing.
+     */
+    void decrease();
+
+    /**
+     * Minimize the rate of incoming requests.
+     */
+    void minimize();
+}
