459bafee68f076f1ac9844da125fb0cfb508f03e
Yu Ning <78631860+Chillax-0v0@users.noreply.github.com>
2024-11-15 10:31:30 +0800

    feat(backpressure): back pressure by system load (#2161)
    * feat(backpressure): init backpressure module
    
    Signed-off-by: Ning Yu <ningyu@automq.com>
    
    * feat(backpressure): implement `DefaultBackPressureManager`
    
    Signed-off-by: Ning Yu <ningyu@automq.com>
    
    * test(backpressure): test `DefaultBackPressureManager`
    
    Signed-off-by: Ning Yu <ningyu@automq.com>
    
    ---------
    
    Signed-off-by: Ning Yu <ningyu@automq.com>

--- s3stream/src/main/java/com/automq/stream/utils/ThreadUtils.java
+++ s3stream/src/main/java/com/automq/stream/utils/ThreadUtils.java
@@ -11,12 +11,13 @@
 
 package com.automq.stream.utils;
 
-import org.slf4j.Logger;
-
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicLong;
-
 import io.netty.util.concurrent.FastThreadLocalThread;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.helpers.NOPLogger;
 
 /**
  * Utilities for working with threads.
@@ -78,5 +79,34 @@ public class ThreadUtils {
                 logger.error("[FATAL] Uncaught exception in executor thread {}", Thread.currentThread().getName(), throwable);
             }
         };
+    }
+
+    /**
+     * A wrapper of {@link #shutdownExecutor} without logging.
+     */
+    public static void shutdownExecutor(ExecutorService executorService, long timeout, TimeUnit timeUnit) {
+        shutdownExecutor(executorService, timeout, timeUnit, NOPLogger.NOP_LOGGER);
+    }
+
+    /**
+     * Shuts down an executor service in two phases, first by calling shutdown to reject incoming tasks,
+     * and then calling shutdownNow, if necessary, to cancel any lingering tasks.
+     * After the timeout/on interrupt, the service is forcefully closed.
+     */
+    public static void shutdownExecutor(ExecutorService executorService, long timeout, TimeUnit timeUnit,
+        Logger logger) {
+        if (null == executorService) {
+            return;
+        }
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(timeout, timeUnit)) {
+                executorService.shutdownNow();
+                logger.error("Executor {} did not terminate in time, forcefully shutting down", executorService);
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
