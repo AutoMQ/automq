dc48293c401de16ad3653b59492b3a99d4a24368
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2025-03-14 14:42:57 +0800

    refactor(automq): rename producerouter to traffic interceptor (#2350)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- core/src/main/java/kafka/automq/interceptor/ClientIdKey.java
+++ core/src/main/java/kafka/automq/interceptor/ClientIdKey.java
@@ -9,7 +9,7 @@
  * by the Apache License, Version 2.0
  */
 
-package kafka.automq.zonerouter;
+package kafka.automq.interceptor;
 
 public class ClientIdKey {
     public static final String AVAILABILITY_ZONE = "automq_az";
