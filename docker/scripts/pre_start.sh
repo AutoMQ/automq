48f6fd445025778d647b81e0967d66be4e109aa8
lingting <sunlisten.gzm@foxmail.com>
2025-03-27 20:52:34 +0800

    feat(docker): Use local code run in docker

---
+++ docker/scripts/pre_start.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+source=""
+target=""
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -s)
+            source="$2"
+            shift 2
+            ;;
+        -t)
+            target="$2"
+            shift 2
+            ;;
+        *)
+            echo "未知的key: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$source" ]; then
+    echo "Error: source invalid" >&2
+    exit 1
+fi
+
+if [ -z "$target" ]; then
+    echo "Error: target invalid" >&2
+    exit 1
+fi
+
+echo "[PreStart] ungzip"
+for f in $source/*.tgz; do
+  tar -xzf "$f" -C "$target" --one-top-level=kafka --strip-components=1 --overwrite
+done
+echo "[PreStart] chmod"
+find "$target" -type f -name "*.sh" -exec chmod a+x {} \;
