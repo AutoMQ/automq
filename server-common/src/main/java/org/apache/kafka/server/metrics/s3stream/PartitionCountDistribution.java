614e078e7b309ef70fe3e06104d269bc51bfb5ed
daniel-y <daniel@automq.com>
2025-04-18 17:12:00 +0800

    chore(license): use the apache license for the next major version
    Signed-off-by: daniel-y <daniel@automq.com>

--- server-common/src/main/java/org/apache/kafka/server/metrics/s3stream/PartitionCountDistribution.java
+++ server-common/src/main/java/org/apache/kafka/server/metrics/s3stream/PartitionCountDistribution.java
@@ -1,12 +1,5 @@
 /*
- * Copyright 2024, AutoMQ CO.,LTD.
- *
- * Use of this software is governed by the Business Source License
- * included in the file BSL.md
- *
- * As of the Change Date specified in that file, in accordance with
- * the Business Source License, use of this software will be governed
- * by the Apache License, Version 2.0
+ * Copyright 2025, AutoMQ HK Limited. Licensed under Apache-2.0.
  */
 
 package org.apache.kafka.server.metrics.s3stream;
