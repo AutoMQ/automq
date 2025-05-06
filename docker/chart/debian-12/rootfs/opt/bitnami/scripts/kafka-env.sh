3b0f6d184bbb71cb8b404c8e4bdc74878f01f87e
1sonofqiu <zhangjianxin@automq.com>
2025-05-06 11:37:39 +0800

    feat(helm/chart): remove unused file

--- docker/chart/debian-12/rootfs/opt/bitnami/scripts/kafka-env.sh
+++ docker/chart/debian-12/rootfs/opt/bitnami/scripts/kafka-env.sh
@@ -8,7 +8,7 @@
 # 1. Custom environment variables defined below after Bitnami defaults
 # 2. Constants defined in this file (environment variables with no default), i.e. BITNAMI_ROOT_DIR
 # 3. Environment variables overridden via external files using *_FILE variables (see below)
-# 4. Environment variables set externally (i.e. current Bash context/bak.bak1.Dockerfile/userdata)
+# 4. Environment variables set externally (i.e. current Bash context/Dockerfile/userdata)
 
 # Load logging library
 # shellcheck disable=SC1090,SC1091
