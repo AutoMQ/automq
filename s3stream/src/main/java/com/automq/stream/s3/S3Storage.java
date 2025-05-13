bb2fc73fd34f139cd562743efa93449e21a45ed5
Robin Han <hanxvdovehx@gmail.com>
2025-05-13 17:08:21 +0800

    feat(zerozone): fast upload when snapshot-read enable
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
+++ s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
@@ -797,7 +797,7 @@ public class S3Storage implements Storage {
         // calculate upload rate
         long elapsed = System.currentTimeMillis() - context.cache.createdTimestamp();
         double rate;
-        if (context.force || elapsed <= 100L) {
+        if (context.force || elapsed <= 100L || config.snapshotReadEnable()) {
             rate = Long.MAX_VALUE;
         } else {
             rate = context.cache.size() * 1000.0 / Math.min(5000L, elapsed);
