95d33fdc6fb9c95d39ff31819185c2e46aabeac4
Gezi-lzq <lzqtxwd@gmail.com>
2025-07-31 10:44:18 +0800

    fix(streamReader): implement scheduled cleanup for expired stream readers (#2719) (#2737)
    * refactor(StreamReader, StreamReaders): inject Time for testable time control and remove reflection from tests
    
    - Add Time dependency to StreamReader and StreamReaders for time-related operations
    - Update constructors to accept Time, defaulting to Time.SYSTEM
    - Replace System.currentTimeMillis() with time.milliseconds() throughout
    - Refactor StreamReadersTest to use MockTime for simulating time passage
    - Remove reflection-based time manipulation in tests for cleaner and safer testing
    
    ---------
    
    Signed-off-by: Gezi-lzq <lzqtxwd@gmail.com>

--- s3stream/src/main/java/com/automq/stream/s3/cache/blockcache/StreamReader.java
+++ s3stream/src/main/java/com/automq/stream/s3/cache/blockcache/StreamReader.java
@@ -26,6 +26,7 @@ import com.automq.stream.s3.model.StreamRecordBatch;
 import com.automq.stream.s3.objects.ObjectManager;
 import com.automq.stream.utils.FutureUtil;
 import com.automq.stream.utils.LogSuppressor;
+import com.automq.stream.utils.Time;
 import com.automq.stream.utils.threads.EventLoop;
 import com.google.common.annotations.VisibleForTesting;
 
@@ -72,19 +73,27 @@ import static com.automq.stream.utils.FutureUtil.exec;
     private final ObjectManager objectManager;
     private final ObjectReaderFactory objectReaderFactory;
     private final DataBlockCache dataBlockCache;
+    private final Time time;
     long nextReadOffset;
     private CompletableFuture<Void> inflightLoadIndexCf;
     private volatile CompletableFuture<Void> afterReadTryReadaheadCf;
-    private long lastAccessTimestamp = System.currentTimeMillis();
+    private long lastAccessTimestamp;
     private boolean reading = false;
 
     private boolean closed = false;
 
     public StreamReader(long streamId, long nextReadOffset, EventLoop eventLoop, ObjectManager objectManager,
         ObjectReaderFactory objectReaderFactory, DataBlockCache dataBlockCache) {
+        this(streamId, nextReadOffset, eventLoop, objectManager, objectReaderFactory, dataBlockCache, Time.SYSTEM);
+    }
+
+    public StreamReader(long streamId, long nextReadOffset, EventLoop eventLoop, ObjectManager objectManager,
+        ObjectReaderFactory objectReaderFactory, DataBlockCache dataBlockCache, Time time) {
         this.streamId = streamId;
         this.nextReadOffset = nextReadOffset;
         this.readahead = new Readahead();
+        this.time = time;
+        this.lastAccessTimestamp = time.milliseconds();
 
         this.eventLoop = eventLoop;
         this.objectManager = objectManager;
@@ -109,7 +118,7 @@ import static com.automq.stream.utils.FutureUtil.exec;
     }
 
     CompletableFuture<ReadDataBlock> read(long startOffset, long endOffset, int maxBytes, int leftRetries) {
-        lastAccessTimestamp = System.currentTimeMillis();
+        lastAccessTimestamp = time.milliseconds();
         ReadContext readContext = new ReadContext();
         read0(readContext, startOffset, endOffset, maxBytes);
         CompletableFuture<ReadDataBlock> retCf = new CompletableFuture<>();
@@ -609,7 +618,7 @@ import static com.automq.stream.utils.FutureUtil.exec;
         private int cacheMissCount;
 
         public void tryReadahead(boolean cacheMiss) {
-            if (System.currentTimeMillis() - resetTimestamp < READAHEAD_RESET_COLD_DOWN_MILLS) {
+            if (time.milliseconds() - resetTimestamp < READAHEAD_RESET_COLD_DOWN_MILLS) {
                 // skip readahead when readahead is in cold down
                 return;
             }
@@ -652,7 +661,7 @@ import static com.automq.stream.utils.FutureUtil.exec;
 
         public void reset() {
             requireReset = true;
-            resetTimestamp = System.currentTimeMillis();
+            resetTimestamp = time.milliseconds();
         }
     }
 
