55dbf25bc00940cb3b1d0560fc70a232c2cc4f81
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2025-08-26 20:24:43 +0800

    chore(config): dynamic change config based on heap size (#2783)

--- core/src/test/java/kafka/automq/zerozone/SnapshotReadPartitionsManagerTest.java
+++ core/src/test/java/kafka/automq/zerozone/SnapshotReadPartitionsManagerTest.java
@@ -62,6 +62,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -152,10 +153,8 @@ public class SnapshotReadPartitionsManagerTest {
         operationBatch.operations.add(snapshotWithOperation(2, Map.of(4L, 6L), SnapshotOperation.ADD));
         subscriber.onNewOperationBatch(operationBatch);
 
-        subscriber.unsafeRun();
-
-        verify(dataLoader, times(1)).replayWal();
-
+        subscriber.run();
+        verify(dataLoader, timeout(1000L).times(1)).replayWal();
         awaitEventLoopClear();
 
         assertEquals(2, subscriber.partitions.size());
