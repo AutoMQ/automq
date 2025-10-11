AutoMQ/automq

commit dd690bec35ec51713713585b8caa6de4dbe1bef5

fix(zerozone2): reset replayer when reset subscriber
Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

core/src/main/java/kafka/automq/zerozone/SubscriberRequester.java
@@ -170,6 +170,10 @@ import io.netty.buffer.Unpooled;
         if (sessionId != 0 && resp.sessionId() != sessionId) {
             // switch to a new session
             subscriber.reset(String.format("switch sessionId from %s to %s", sessionId, resp.sessionId()));
+            // reset immediately to the new session.
+            tryReset0();
+            sessionId = resp.sessionId();
+            sessionEpoch = resp.sessionEpoch();
         }
         SnapshotReadPartitionsManager.OperationBatch batch = new SnapshotReadPartitionsManager.OperationBatch();
         resp.topics().forEach(topic -> topic.partitions().forEach(partition -> {
@@ -190,8 +194,6 @@ import io.netty.buffer.Unpooled;
         subscriber.onNewWalEndOffset(resp.confirmWalConfig(), DefaultRecordOffset.of(Unpooled.wrappedBuffer(resp.confirmWalEndOffset())));
         batch.operations.add(SnapshotWithOperation.snapshotMark(snapshotCf));
         subscriber.onNewOperationBatch(batch);
-        sessionId = resp.sessionId();
-        sessionEpoch = resp.sessionEpoch();
     }
 
     private boolean tryReset0() {

core/src/main/java/kafka/automq/partition/snapshot/PartitionSnapshotsManager.java
@@ -101,6 +101,7 @@ public class PartitionSnapshotsManager {
 
     public CompletableFuture<AutomqGetPartitionSnapshotResponse> handle(AutomqGetPartitionSnapshotRequest request) {
         Session session;
+        boolean newSession = false;
         synchronized (this) {
             AutomqGetPartitionSnapshotRequestData requestData = request.data();
             int sessionId = requestData.sessionId();
@@ -115,10 +116,11 @@ public class PartitionSnapshotsManager {
                 sessionId = nextSessionId();
                 session = new Session(sessionId);
                 sessions.put(sessionId, session);
+                newSession = true;
             }
         }
         CompletableFuture<AutomqGetPartitionSnapshotResponse> resp = session.snapshotsDelta(request.data().version());
-        CompletableFuture<Void> commitCf = request.data().requestCommit() ? confirmWAL.commit(0, false) : CompletableFuture.completedFuture(null);
+        CompletableFuture<Void> commitCf = (request.data().requestCommit() || newSession) ? confirmWAL.commit(0, false) : CompletableFuture.completedFuture(null);
         return commitCf.exceptionally(nil -> null).thenCompose(nil -> resp);
     }
 

core/src/main/java/kafka/automq/zerozone/SnapshotReadPartitionsManager.java
@@ -360,6 +360,7 @@ public class SnapshotReadPartitionsManager implements MetadataListener, ProxyTop
             snapshotWithOperations.clear();
             waitingDataLoadedQueue.clear();
             requester.reset();
+            replayer.reset();
         }
 
         void onNewWalEndOffset(String walConfig, RecordOffset endOffset) {

core/src/main/java/kafka/automq/zerozone/SubscriberReplayer.java
@@ -111,6 +111,11 @@ class SubscriberReplayer {
         }
     }
 
+    public void reset() {
+        this.loadedObjectOrderId = -1L;
+        this.loadedEndOffset = null;
+    }
+
     private List<S3ObjectMetadata> nextObjects() {
         return nextObjects0(metadataCache, node.id(), loadedObjectOrderId, value -> loadedObjectOrderId = value);
     }

commit d1b5b263a0693c30f0dfa2bc2015116b8f8a7cbf

fix(zerozone2): fix bug caused by #2929
Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

core/src/main/java/kafka/automq/zerozone/SubscriberRequester.java
@@ -172,9 +172,9 @@ import io.netty.buffer.Unpooled;
             subscriber.reset(String.format("switch sessionId from %s to %s", sessionId, resp.sessionId()));
             // reset immediately to the new session.
             tryReset0();
-            sessionId = resp.sessionId();
-            sessionEpoch = resp.sessionEpoch();
         }
+        sessionId = resp.sessionId();
+        sessionEpoch = resp.sessionEpoch();
         SnapshotReadPartitionsManager.OperationBatch batch = new SnapshotReadPartitionsManager.OperationBatch();
         resp.topics().forEach(topic -> topic.partitions().forEach(partition -> {
             String topicName = topicNameGetter.apply(topic.topicId());