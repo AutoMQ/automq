AutoMQ/automq

commit fc5e335a49daad70147038b19126744461566016

feat(zerozone2): S3WAL supports sequential append and distributed read (#2764)
- S3WAL supports sequential append & callback
- Simple the S3Storage logic by S3WAl sequential append
- S3WAL supports distributed read from another nodes
- S3Storage supports linked record

Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

s3stream/src/main/java/com/automq/stream/s3/operator/AwsObjectStorage.java
@@ -19,7 +19,6 @@
 
 package com.automq.stream.s3.operator;
 
-import com.automq.stream.s3.ByteBufAlloc;
 import com.automq.stream.s3.exceptions.ObjectNotExistException;
 import com.automq.stream.s3.metrics.operations.S3Operation;
 import com.automq.stream.s3.network.NetworkBandwidthLimiter;
@@ -181,7 +180,7 @@ public class AwsObjectStorage extends AbstractObjectStorage {
         CompletableFuture<ByteBuf> cf = new CompletableFuture<>();
         readS3Client.getObject(builder.build(), AsyncResponseTransformer.toPublisher())
             .thenAccept(responsePublisher -> {
-                CompositeByteBuf buf = ByteBufAlloc.compositeByteBuffer();
+                CompositeByteBuf buf = Unpooled.compositeBuffer();
                 responsePublisher.subscribe(bytes -> {
                     // the aws client will copy DefaultHttpContent to heap ByteBuffer
                     buf.addComponent(true, Unpooled.wrappedBuffer(bytes));

core/src/main/java/kafka/automq/zerozone/SnapshotReadPartitionsManager.java
@@ -51,7 +51,7 @@ import org.apache.kafka.metadata.stream.S3Object;
 import org.apache.kafka.metadata.stream.S3StreamSetObject;
 import org.apache.kafka.storage.internals.log.LogOffsetMetadata;
 
-import com.automq.stream.s3.cache.SnapshotReadCache;
+import com.automq.stream.Context;
 import com.automq.stream.s3.metadata.S3ObjectMetadata;
 import com.automq.stream.utils.Threads;
 import com.automq.stream.utils.threads.EventLoop;
@@ -103,7 +103,8 @@ public class SnapshotReadPartitionsManager implements MetadataListener, ProxyTop
         this.time = time;
         this.replicaManager = replicaManager;
         this.metadataCache = metadataCache;
-        this.dataLoader = objects -> SnapshotReadCache.instance().load(objects);
+        // FIXME: tmp code in merging
+        this.dataLoader = objects -> Context.instance().snapshotReadCache().replay(objects);
         this.asyncSender = new AsyncSender.BrokersAsyncSender(config, metrics, "snapshot_read", Time.SYSTEM, "AUTOMQ_SNAPSHOT_READ", new LogContext());
     }
 

core/src/main/scala/kafka/log/stream/s3/wal/BootstrapWalV1.java
@@ -25,8 +25,11 @@ import org.apache.kafka.common.utils.ThreadUtils;
 import org.apache.kafka.controller.stream.NodeMetadata;
 
 import com.automq.stream.s3.exceptions.AutoMQException;
+import com.automq.stream.s3.model.StreamRecordBatch;
 import com.automq.stream.s3.trace.context.TraceContext;
 import com.automq.stream.s3.wal.AppendResult;
+import com.automq.stream.s3.wal.OpenMode;
+import com.automq.stream.s3.wal.RecordOffset;
 import com.automq.stream.s3.wal.RecoverResult;
 import com.automq.stream.s3.wal.WalFactory;
 import com.automq.stream.s3.wal.WalFactory.BuildOptions;
@@ -44,11 +47,12 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import io.netty.buffer.ByteBuf;
+
 
 public class BootstrapWalV1 implements WriteAheadLog {
     private static final Logger LOGGER = LoggerFactory.getLogger(BootstrapWalV1.class);
@@ -114,8 +118,23 @@ public class BootstrapWalV1 implements WriteAheadLog {
     }
 
     @Override
-    public AppendResult append(TraceContext context, ByteBuf data, int crc) throws OverCapacityException {
-        return wal.append(context, data, crc);
+    public CompletableFuture<AppendResult> append(TraceContext context, StreamRecordBatch streamRecordBatch) throws OverCapacityException {
+        return wal.append(context, streamRecordBatch);
+    }
+
+    @Override
+    public CompletableFuture<StreamRecordBatch> get(RecordOffset recordOffset) {
+        return wal.get(recordOffset);
+    }
+
+    @Override
+    public CompletableFuture<List<StreamRecordBatch>> get(RecordOffset startOffset, RecordOffset endOffset) {
+        return wal.get(startOffset, endOffset);
+    }
+
+    @Override
+    public RecordOffset confirmOffset() {
+        return wal.confirmOffset();
     }
 
     @Override
@@ -157,7 +176,7 @@ public class BootstrapWalV1 implements WriteAheadLog {
     }
 
     @Override
-    public CompletableFuture<Void> trim(long offset) {
+    public CompletableFuture<Void> trim(RecordOffset offset) {
         return wal.trim(offset);
     }
 
@@ -165,7 +184,7 @@ public class BootstrapWalV1 implements WriteAheadLog {
         IdURI uri = IdURI.parse(kraftWalConfigs);
         CompletableFuture<Void> cf = walHandle
             .acquirePermission(nodeId, oldNodeEpoch, uri, new WalHandle.AcquirePermissionOptions().failoverMode(failoverMode));
-        return cf.thenApplyAsync(nil -> factory.build(uri, BuildOptions.builder().nodeEpoch(oldNodeEpoch).failoverMode(failoverMode).build()), executor);
+        return cf.thenApplyAsync(nil -> factory.build(uri, BuildOptions.builder().nodeEpoch(oldNodeEpoch).openMode(failoverMode ? OpenMode.FAILOVER : OpenMode.READ_WRITE).build()), executor);
     }
 
     private CompletableFuture<? extends WriteAheadLog> buildWal(String kraftWalConfigs) {
@@ -175,7 +194,7 @@ public class BootstrapWalV1 implements WriteAheadLog {
             .failoverMode(false);
         CompletableFuture<Void> cf = walHandle
             .acquirePermission(nodeId, nodeEpoch, uri, options);
-        return cf.thenApplyAsync(nil -> factory.build(uri, BuildOptions.builder().nodeEpoch(nodeEpoch).failoverMode(false).build()), executor);
+        return cf.thenApplyAsync(nil -> factory.build(uri, BuildOptions.builder().nodeEpoch(nodeEpoch).openMode(OpenMode.READ_WRITE).build()), executor);
     }
 
     private CompletableFuture<Void> releasePermission(String kraftWalConfigs) {

core/src/main/scala/kafka/log/stream/s3/wal/DefaultWalFactory.java
@@ -24,8 +24,10 @@ import com.automq.stream.s3.network.NetworkBandwidthLimiter;
 import com.automq.stream.s3.operator.BucketURI;
 import com.automq.stream.s3.operator.ObjectStorage;
 import com.automq.stream.s3.operator.ObjectStorageFactory;
+import com.automq.stream.s3.wal.ReservationService;
 import com.automq.stream.s3.wal.WalFactory;
 import com.automq.stream.s3.wal.WriteAheadLog;
+import com.automq.stream.s3.wal.impl.object.ObjectReservationService;
 import com.automq.stream.s3.wal.impl.object.ObjectWALConfig;
 import com.automq.stream.s3.wal.impl.object.ObjectWALService;
 import com.automq.stream.utils.IdURI;
@@ -65,8 +67,9 @@ public class DefaultWalFactory implements WalFactory {
                     .withClusterId(AutoMQApplication.getClusterId())
                     .withNodeId(nodeId)
                     .withEpoch(options.nodeEpoch())
-                    .withFailover(options.failoverMode());
-
+                    .withOpenMode(options.openMode());
+                ReservationService reservationService = new ObjectReservationService(AutoMQApplication.getClusterId(), walObjectStorage, walObjectStorage.bucketId());
+                configBuilder.withReservationService(reservationService);
                 return new ObjectWALService(Time.SYSTEM, walObjectStorage, configBuilder.build());
             default:
                 throw new IllegalArgumentException("Unsupported WAL protocol: " + uri.protocol());

core/src/main/scala/kafka/log/streamaspect/ClientWrapper.java
@@ -232,6 +232,11 @@ public class ClientWrapper implements Client {
         }
 
         @Override
+        public void confirmOffset(long offset) {
+            stream.confirmOffset(offset);
+        }
+
+        @Override
         public long nextOffset() {
             return stream.nextOffset();
         }

core/src/main/scala/kafka/log/streamaspect/LazyStream.java
@@ -115,6 +115,11 @@ public class LazyStream implements Stream {
     }
 
     @Override
+    public void confirmOffset(long offset) {
+        inner.confirmOffset(offset);
+    }
+
+    @Override
     public long nextOffset() {
         return inner.nextOffset();
     }
@@ -197,6 +202,10 @@ public class LazyStream implements Stream {
         @Override
         public long confirmOffset() {
             return 0;
+        }
+
+        @Override
+        public void confirmOffset(long offset) {
         }
 
         @Override

core/src/main/scala/kafka/log/streamaspect/MemoryClient.java
@@ -110,6 +110,11 @@ public class MemoryClient implements Client {
         }
 
         @Override
+        public void confirmOffset(long offset) {
+            nextOffsetAlloc.set(offset);
+        }
+
+        @Override
         public long nextOffset() {
             return nextOffsetAlloc.get();
         }

core/src/main/scala/kafka/log/streamaspect/MetaStream.java
@@ -118,6 +118,11 @@ public class MetaStream implements Stream {
     }
 
     @Override
+    public void confirmOffset(long offset) {
+        innerStream.confirmOffset(offset);
+    }
+
+    @Override
     public long nextOffset() {
         return innerStream.nextOffset();
     }

commit 7a5ca46201c657f0524fdf3f8014357c8ca00bc4

perf(s3stream): optimize the doRangeRead GC issue caused by #2764
Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

s3stream/src/main/java/com/automq/stream/s3/operator/AwsObjectStorage.java
@@ -180,7 +180,8 @@ public class AwsObjectStorage extends AbstractObjectStorage {
         CompletableFuture<ByteBuf> cf = new CompletableFuture<>();
         readS3Client.getObject(builder.build(), AsyncResponseTransformer.toPublisher())
             .thenAccept(responsePublisher -> {
-                CompositeByteBuf buf = Unpooled.compositeBuffer();
+                // Set maxNumComponents to Integer.MAX_VALUE to avoid #consolidateIfNeeded causing a GC issue.
+                CompositeByteBuf buf = Unpooled.compositeBuffer(Integer.MAX_VALUE);
                 responsePublisher.subscribe(bytes -> {
                     // the aws client will copy DefaultHttpContent to heap ByteBuffer
                     buf.addComponent(true, Unpooled.wrappedBuffer(bytes));