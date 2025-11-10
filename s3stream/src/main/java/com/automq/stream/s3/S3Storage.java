f28e242143a26fb59099dfb8a5ea1818f8fbacc0
Xu Han@AutoMQ <hanxvdovehx@gmail.com>
2025-11-10 20:17:51 +0800

    perf(s3stream): parallel append & optimize logcache init (#2995)
    Signed-off-by: Robin Han <hanxvdovehx@gmail.com>

--- s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
+++ s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
@@ -48,8 +48,10 @@ import com.automq.stream.s3.wal.exception.OverCapacityException;
 import com.automq.stream.utils.ExceptionUtil;
 import com.automq.stream.utils.FutureTicker;
 import com.automq.stream.utils.FutureUtil;
+import com.automq.stream.utils.Systems;
 import com.automq.stream.utils.ThreadUtils;
 import com.automq.stream.utils.Threads;
+import com.automq.stream.utils.threads.EventLoop;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
@@ -89,7 +91,6 @@ import java.util.stream.IntStream;
 import io.opentelemetry.instrumentation.annotations.SpanAttribute;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 
-import static com.automq.stream.utils.FutureUtil.suppress;
 
 public class S3Storage implements Storage {
     private static final Logger LOGGER = LoggerFactory.getLogger(S3Storage.class);
@@ -152,6 +153,9 @@ public class S3Storage implements Storage {
      * @see #handleAppendCallback
      */
     private final Lock[] streamCallbackLocks = IntStream.range(0, NUM_STREAM_CALLBACK_LOCKS).mapToObj(i -> new ReentrantLock()).toArray(Lock[]::new);
+    private final EventLoop[] callbackExecutors = IntStream.range(0, Systems.CPU_CORES).mapToObj(i -> new EventLoop("AUTOMQ_S3STREAM_APPEND_CALLBACK-" + i))
+        .toArray(EventLoop[]::new);
+
     private long lastLogTimestamp = 0L;
     private volatile double maxDataWriteRate = 0.0;
 
@@ -531,6 +535,9 @@ public class S3Storage implements Storage {
             backgroundExecutor.shutdownNow();
             LOGGER.warn("await backgroundExecutor close fail", e);
         }
+        for (EventLoop executor : callbackExecutors) {
+            executor.shutdownGracefully();
+        }
     }
 
     @Override
@@ -808,7 +815,15 @@ public class S3Storage implements Storage {
     }
 
     private void handleAppendCallback(WalWriteRequest request) {
-        suppress(() -> handleAppendCallback0(request), LOGGER);
+        // parallel execute append callback in streamId based executor.
+        EventLoop executor = callbackExecutors[Math.abs((int) (request.record.getStreamId() % callbackExecutors.length))];
+        executor.execute(() -> {
+            try {
+                handleAppendCallback0(request);
+            } catch (Throwable e) {
+                LOGGER.error("[UNEXPECTED], handle append callback fail, request {}", request, e);
+            }
+        });
     }
 
     private void handleAppendCallback0(WalWriteRequest request) {
@@ -820,7 +835,6 @@ public class S3Storage implements Storage {
             // cache block is full, trigger WAL upload.
             uploadDeltaWAL();
         }
-        // TODO: parallel callback
         request.cf.complete(null);
         StorageOperationStats.getInstance().appendCallbackStats.record(TimerUtil.timeElapsedSince(startTime, TimeUnit.NANOSECONDS));
     }
