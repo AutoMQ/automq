54481b87294a8e53b1af35690c65ab50224df9e9
Gezi-lzq <lzqtxwd@gmail.com>
2026-07-31 03:28:22 +0000

    refactor(s3stream): scope fetch contexts per operation

--- s3stream/src/main/java/com/automq/stream/s3/S3Stream.java
+++ s3stream/src/main/java/com/automq/stream/s3/S3Stream.java
@@ -24,7 +24,6 @@ import com.automq.stream.RecordBatchWithContextWrapper;
 import com.automq.stream.api.AppendResult;
 import com.automq.stream.api.FetchResult;
 import com.automq.stream.api.OpenStreamOptions;
-import com.automq.stream.api.ReadOptions;
 import com.automq.stream.api.RecordBatch;
 import com.automq.stream.api.RecordBatchWithContext;
 import com.automq.stream.api.Stream;
@@ -243,15 +242,13 @@ public class S3Stream implements Stream, StreamMetadataListener {
         @SpanAttribute long startOffset,
         @SpanAttribute long endOffset,
         @SpanAttribute int maxBytes) {
-        FetchContext effectiveContext = new FetchContext(context);
-        ReadOptions effectiveReadOptions = snapshotRead()
-            ? context.readOptions().withSnapshotRead(true)
-            : context.readOptions();
-        effectiveContext.setReadOptions(effectiveReadOptions);
+        if (snapshotRead()) {
+            context.setReadOptions(context.readOptions().withSnapshotRead(true));
+        }
         TimerUtil timerUtil = new TimerUtil();
         readLock.lock();
         try {
-            CompletableFuture<FetchResult> cf = exec(() -> fetch0(effectiveContext, startOffset, endOffset, maxBytes), LOGGER, "fetch");
+            CompletableFuture<FetchResult> cf = exec(() -> fetch0(context, startOffset, endOffset, maxBytes), LOGGER, "fetch");
             CompletableFuture<FetchResult> retCf = cf.thenApply(rs -> {
                 // TODO: move the fast / slow read metrics to kafka module.
                 long totalSize = 0L;
@@ -259,7 +256,7 @@ public class S3Stream implements Stream, StreamMetadataListener {
                     totalSize += recordBatch.rawPayload().remaining();
                 }
                 final long finalSize = totalSize;
-                if (effectiveContext.readOptions().fastRead()) {
+                if (context.readOptions().fastRead()) {
                     NetworkStats.getInstance().fastReadBytesStats(streamId).ifPresent(counter -> counter.inc(finalSize));
                 } else {
                     NetworkStats.getInstance().slowReadBytesStats(streamId).ifPresent(counter -> counter.inc(finalSize));
